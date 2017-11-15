3be0f4db0ffe34c52bc33c4d0132cb2d4203d4c3
u225675 <u225675@sbb.ch>
2017-11-15 17:35:10 +0100

    whitespace

--- src/main/java/ch/sbb/matsim/preparation/PopulationMerger.java
+++ src/main/java/ch/sbb/matsim/preparation/PopulationMerger.java
@@ -5,7 +5,6 @@
 package ch.sbb.matsim.preparation;
 
 import ch.sbb.matsim.config.PopulationMergerConfigGroup;
-import ch.sbb.matsim.config.PtMergerConfigGroup;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Person;
@@ -19,32 +18,28 @@ import org.matsim.utils.objectattributes.ObjectAttributes;
 import org.matsim.utils.objectattributes.ObjectAttributesXmlReader;
 import org.matsim.utils.objectattributes.ObjectAttributesXmlWriter;
 
-import java.io.File;
-
 public class PopulationMerger {
     public static void main(final String[] args) {
-        Logger log = Logger.getLogger(Cutter.class);
-        PopulationReader reader;
-        PopulationWriter writer;
-        ObjectAttributesXmlWriter attributesWriter;
-
         final Config config = ConfigUtils.loadConfig(args[0], new PopulationMergerConfigGroup());
-
-        Scenario scenario = ScenarioUtils.createScenario(config);
-        ObjectAttributes personAttributes = scenario.getPopulation().getPersonAttributes();
-
-        reader = new PopulationReaderMatsimV5(scenario);
-        reader.readFile(config.plans().getInputFile());
-        new ObjectAttributesXmlReader(scenario.getPopulation().getPersonAttributes()).parse(config.plans().getInputPersonAttributeFile());
-
         final PopulationMergerConfigGroup mergerConfig = (PopulationMergerConfigGroup) config.getModule(PopulationMergerConfigGroup.GROUP_NAME);
+
+        final Scenario scenario = ScenarioUtils.createScenario(config);
+        final PopulationReader reader = new PopulationReaderMatsimV5(scenario);
+        final PopulationWriter writer = new PopulationWriter(scenario.getPopulation());
+
+        final ObjectAttributes personAttributes = scenario.getPopulation().getPersonAttributes();
+        final ObjectAttributesXmlReader attributesReader = new ObjectAttributesXmlReader(personAttributes);
+        final ObjectAttributesXmlWriter attributesWriter = new ObjectAttributesXmlWriter(personAttributes);;
+
+        reader.readFile(config.plans().getInputFile());
+        attributesReader.parse(config.plans().getInputPersonAttributeFile());
 
         String inputPlanFile = null;
         while ((inputPlanFile = mergerConfig.shiftInputPlansFiles()) != null) {
             reader.readFile(inputPlanFile);
         }
 
-        for (Person person : scenario.getPopulation().getPersons().values()) {
+        for (final Person person : scenario.getPopulation().getPersons().values()) {
             if (personAttributes.getAttribute(person.getId().toString(), mergerConfig.getMergedPersonAttributeKey()) == null) {
                 personAttributes.putAttribute(
                         person.getId().toString(),
@@ -54,10 +49,7 @@ public class PopulationMerger {
             }
         }
 
-        writer = new PopulationWriter(scenario.getPopulation());
         writer.write(mergerConfig.getOutputPlansFile());
-
-        attributesWriter = new ObjectAttributesXmlWriter(personAttributes);
         attributesWriter.writeFile(mergerConfig.getOutputPersonAttributesFile());
     }
 }
