7f28d6d1810671336f3028ca4819c9c66532f740
u225675 <u225675@sbb.ch>
2017-11-16 16:04:48 +0100

    add custom attributes to all persons

--- src/main/java/ch/sbb/matsim/preparation/PopulationMerger.java
+++ src/main/java/ch/sbb/matsim/preparation/PopulationMerger.java
@@ -9,6 +9,7 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.population.PersonUtils;
 import org.matsim.core.population.PopulationReader;
 import org.matsim.core.population.PopulationReaderMatsimV5;
 import org.matsim.core.population.PopulationWriter;
@@ -17,8 +18,10 @@ import org.matsim.utils.objectattributes.ObjectAttributes;
 import org.matsim.utils.objectattributes.ObjectAttributesXmlReader;
 import org.matsim.utils.objectattributes.ObjectAttributesXmlWriter;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PopulationMerger {
-    private PopulationMergerConfigGroup mergerConfig;
     private Scenario scenario;
     private PopulationReader reader;
     private PopulationWriter writer;
@@ -28,17 +31,21 @@ public class PopulationMerger {
 
     public static void main(final String[] args) {
         Config config = ConfigUtils.loadConfig(args[0], new PopulationMergerConfigGroup());
+        PopulationMergerConfigGroup mergerConfig = (PopulationMergerConfigGroup) config.getModule(PopulationMergerConfigGroup.GROUP_NAME);
 
         PopulationMerger merger = new PopulationMerger(config);
 
-        merger.mergeInputPlanFiles();
-        merger.putPersonAttributes();
-        merger.writeOutputFiles();
+        Map<String, String> attributes = new TreeMap<String, String>();
+        attributes.put(mergerConfig.getMergedPersonAttributeKey(), mergerConfig.getMergedPersonAttributeValue());
+        attributes.put("season_ticket", "none");
+
+        merger.mergeInputPlanFiles(mergerConfig);
+        merger.putPersonAttributes(attributes);
+        merger.putPersonCustomAttributes();
+        merger.writeOutputFiles(mergerConfig);
     }
 
     public PopulationMerger(Config config) {
-        this.mergerConfig = (PopulationMergerConfigGroup) config.getModule(PopulationMergerConfigGroup.GROUP_NAME);
-
         this.scenario = ScenarioUtils.createScenario(config);
         this.reader = new PopulationReaderMatsimV5(this.scenario);
         this.writer = new PopulationWriter(this.scenario.getPopulation());
@@ -51,27 +58,53 @@ public class PopulationMerger {
         this.attributesReader.parse(config.plans().getInputPersonAttributeFile());
     }
 
-    protected void mergeInputPlanFiles() {
+    protected void mergeInputPlanFiles(PopulationMergerConfigGroup mergerConfig) {
         String inputPlanFile = null;
-        while ((inputPlanFile = this.mergerConfig.shiftInputPlansFiles()) != null) {
+        while ((inputPlanFile = mergerConfig.shiftInputPlansFiles()) != null) {
             this.reader.readFile(inputPlanFile);
         }
     }
 
-    protected void putPersonAttributes() {
+    protected void putPersonAttributes(Map<String, String> attributes) {
         for (final Person person : this.scenario.getPopulation().getPersons().values()) {
-            if (this.personAttributes.getAttribute(person.getId().toString(), this.mergerConfig.getMergedPersonAttributeKey()) == null) {
-                this.personAttributes.putAttribute(
-                        person.getId().toString(),
-                        this.mergerConfig.getMergedPersonAttributeKey(),
-                        this.mergerConfig.getMergedPersonAttributeValue()
-                );
+            for (Map.Entry<String, String> entry : attributes.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                if (this.personAttributes.getAttribute(person.getId().toString(), key) == null) {
+                    this.personAttributes.putAttribute(person.getId().toString(), key, value);
+                }
             }
         }
     }
 
-    protected void writeOutputFiles() {
-        this.writer.write(this.mergerConfig.getOutputPlansFile());
-        this.attributesWriter.writeFile(this.mergerConfig.getOutputPersonAttributesFile());
+    protected void putPersonCustomAttributes() {
+        for (final Person person : this.scenario.getPopulation().getPersons().values()) {
+            Object carAvail = this.personAttributes.getAttribute(person.getId().toString(), "availability: car");
+
+            if (carAvail != null && (!carAvail.toString().equals("never"))) {
+                person.getCustomAttributes().put("carAvail", "always");
+                PersonUtils.setLicence(person, "yes");
+            } else{
+                person.getCustomAttributes().put("carAvail", "never");
+                PersonUtils.setLicence(person, "no");
+            }
+
+            Object age = this.personAttributes.getAttribute(person.getId().toString(), "age");
+
+            if (age != null){
+                person.getCustomAttributes().put("age", age);
+            }
+
+            Object gender = this.personAttributes.getAttribute(person.getId().toString(), "gender");
+
+            if (gender != null && (!gender.toString().isEmpty())){
+                person.getCustomAttributes().put("gender", gender.toString());
+            }
+        }
+    }
+
+    protected void writeOutputFiles(PopulationMergerConfigGroup mergerConfig) {
+        this.writer.write(mergerConfig.getOutputPlansFile());
+        this.attributesWriter.writeFile(mergerConfig.getOutputPersonAttributesFile());
     }
 }
