c5c180b882672fe68ccdaf4c2c2e9368393fa059
Manser Patrick (P-V-APL-MEP) <patrick.manser@sbb.ch>
2018-06-04 20:29:14 +0200

    sampler

--- src/main/java/ch/sbb/matsim/preparation/PopulationSampler/SBBPopulationSampler.java
+++ src/main/java/ch/sbb/matsim/preparation/PopulationSampler/SBBPopulationSampler.java
@@ -11,23 +11,29 @@ import org.matsim.core.gbl.MatsimRandom;
 import org.matsim.core.population.io.PopulationReader;
 import org.matsim.core.population.io.PopulationWriter;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.utils.objectattributes.ObjectAttributesXmlReader;
+import org.matsim.utils.objectattributes.ObjectAttributesXmlWriter;
 
 
 public class SBBPopulationSampler {
 
     public static void main(String[] args)  {
         String inputPopulation = args[0];
-        double fraction = Double.valueOf(args[1]);
-        String outputPopulation = args[2];
+        String inputAttributes = args[1];
+        double fraction = Double.valueOf(args[2]);
+        String outputPopulation = args[3];
+        String outputAttributes = args[4];
 
         Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
 
         new PopulationReader(scenario).readFile(inputPopulation);
+        new ObjectAttributesXmlReader(scenario.getPopulation().getPersonAttributes()).readFile(inputAttributes);
 
         SBBPopulationSampler sampler = new SBBPopulationSampler();
         sampler.sample(scenario.getPopulation(), fraction);
 
         new PopulationWriter(scenario.getPopulation()).write(outputPopulation);
+        new ObjectAttributesXmlWriter(scenario.getPopulation().getPersonAttributes()).writeFile(outputAttributes);
     }
 
     public void sample(Population population, double fraction) {
@@ -44,6 +50,7 @@ public class SBBPopulationSampler {
 
         for (Id<Person> pId : toDelete) {
             population.removePerson(pId);
+            population.getPersonAttributes().removeAllAttributes(pId.toString());
         }
     }
 }
