e36e48029a539b51eb7d9bc46b06e6041eac9d93
u225744 <patrick.manser@sbb.ch>
2018-12-04 07:28:08 +0100

    remove comment

--- src/main/java/ch/sbb/matsim/plans/abm/AbmConverter.java
+++ src/main/java/ch/sbb/matsim/plans/abm/AbmConverter.java
@@ -100,7 +100,6 @@ public class AbmConverter {
             final Id<Person> pId = Id.createPersonId(person.getId().toString().replace("P_", ""));
             final Person synpopPerson = synpopPopulation.getPersons().get(pId);
             if (synpopPerson != null) {
-                // PM: in my opinion, all those attributes are not necessary
                 /*
                 for (final Map.Entry<String, Object> entry : synpopPerson.getAttributes().getAsMap().entrySet()) {
                     person.getAttributes().putAttribute(entry.getKey(), entry.getValue());
