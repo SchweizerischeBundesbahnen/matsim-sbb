99e55aff34e0bf070bff9bc72bed1b55278f4d35
Métrailler Denis (P-FV-APL-MEP) <denis.metrailler@sbb.ch>
2019-01-07 13:26:58 +0100

    copied from test_annick + small changes

---
+++ src/main/java/ch/sbb/matsim/mavi/streets/NetworkCleaner.java
@@ -0,0 +1,22 @@
+package ch.sbb.matsim.mavi.streets;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.io.MatsimNetworkReader;
+import org.matsim.core.network.io.NetworkWriter;
+import org.matsim.core.scenario.ScenarioUtils;
+
+public class NetworkCleaner {
+    public static void main(String[] args) {
+
+
+        String input = args[0];
+        String output = args[1];
+
+        Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        new MatsimNetworkReader(scenario.getNetwork()).readFile(input);
+        org.matsim.core.network.algorithms.NetworkCleaner cleaner = new org.matsim.core.network.algorithms.NetworkCleaner();
+        cleaner.run(scenario.getNetwork());
+        new NetworkWriter(scenario.getNetwork()).write(output);
+    }
+}
