2bac8c3f81db839401f75cf3af301324a8ef5051
Marcel Rieser <rieser@simunto.com>
2019-02-21 16:53:09 +0100

    performance improvement by also caching null values (e.g. no zone found)

--- src/main/java/ch/sbb/matsim/zones/ZonesQueryCache.java
+++ src/main/java/ch/sbb/matsim/zones/ZonesQueryCache.java
@@ -2,13 +2,13 @@ package ch.sbb.matsim.zones;
 
 import org.matsim.api.core.v01.Coord;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class ZonesQueryCache {
 
     private final Zones zones;
-    private final Map<Coord, Zone> cache = new ConcurrentHashMap<>();
+    private final Map<Coord, Zone> cache = new HashMap<>();
 
     public ZonesQueryCache(Zones zones) {
         this.zones = zones;
@@ -16,6 +16,11 @@ public class ZonesQueryCache {
 
     public Zone findZone(double x, double y) {
         Coord c = new Coord(x, y);
-        return this.cache.computeIfAbsent(c, k -> zones.findZone(x, y));
+        if (this.cache.containsKey(c)) {
+            return this.cache.get(c);
+        }
+        Zone z = this.zones.findZone(x, y);
+        this.cache.put(c, z);
+        return z;
     }
 }
