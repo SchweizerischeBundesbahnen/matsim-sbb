710a146f022fb5aabf22a401321b15bb1fe2bdd8
u225744 <u225744@sbb.ch>
2019-05-22 17:56:28 +0200

    export link sequences from visum

--- src/main/java/ch/sbb/matsim/mavi/pt/TimeProfileExporter.java
+++ src/main/java/ch/sbb/matsim/mavi/pt/TimeProfileExporter.java
@@ -1,5 +1,6 @@
 package ch.sbb.matsim.mavi.pt;
 
+import ch.sbb.matsim.csv.CSVWriter;
 import ch.sbb.matsim.mavi.visum.Visum;
 import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type;
 import org.apache.log4j.Logger;
@@ -19,6 +20,7 @@ import org.matsim.vehicles.VehicleType;
 import org.matsim.vehicles.Vehicles;
 import org.matsim.vehicles.VehiclesFactory;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.IntStream;
 
@@ -32,6 +34,7 @@ public class TimeProfileExporter {
     private final VehiclesFactory vehicleBuilder;
     private Network network;
     private TransitSchedule schedule;
+    private HashMap<Id<Link>, String> linkToVisumSequence = new HashMap<>();
 
     public TimeProfileExporter(Scenario scenario)   {
         this.network = scenario.getNetwork();
@@ -78,6 +81,7 @@ public class TimeProfileExporter {
                     Id<Link> startLink = null;
                     Id<Link> endLink = null;
                     TransitStopFacility fromStop = null;
+                    String prevLinkSeq = null;
                     double postlength = 0.0;
                     double delta = 0.0;
                     boolean isFirstRouteStop = true;
@@ -118,32 +122,38 @@ public class TimeProfileExporter {
                             Id<Link> newLinkID = Id.createLinkId(fromNode.getId().toString() + "-" + toNode.getId().toString());
                             if (!this.network.getLinks().containsKey(newLinkID)) {
                                 createLink(newLinkID, fromNode, toNode, mode, postlength);
+                                this.linkToVisumSequence.put(newLinkID, prevLinkSeq);
                             }
                             // differentiate between links with the same from- and to-node but different length
                             else {
-                                boolean hasLinkWithSameLength = false;
-                                if (this.network.getLinks().get(newLinkID).getLength() != postlength * 1000) {
+                                boolean hasSameLinkSequence = false;
+                                if(!this.linkToVisumSequence.get(newLinkID).equals(prevLinkSeq)) {
                                     int m = 1;
                                     Id<Link> linkID = Id.createLinkId(fromNode.getId().toString() + "-" + toNode.getId().toString() + "." + m);
                                     while (this.network.getLinks().containsKey(linkID)) {
-                                        if (this.network.getLinks().get(linkID).getLength() == postlength * 1000) {
-                                            hasLinkWithSameLength = true;
+                                        if (this.linkToVisumSequence.get(linkID).equals(prevLinkSeq)) {
+                                            hasSameLinkSequence = true;
+                                            newLinkID = linkID;
                                             break;
                                         }
                                         m++;
                                         linkID = Id.createLinkId(fromNode.getId().toString() + "-" + toNode.getId().toString() + "." + m);
+
                                     }
-                                    if (!hasLinkWithSameLength) {
+                                    if (!hasSameLinkSequence) {
                                         createLink(linkID, fromNode, toNode, mode, postlength);
+                                        this.linkToVisumSequence.put(linkID, prevLinkSeq);
                                         newLinkID = linkID;
                                     }
                                 }
                             }
                             routeLinks.add(newLinkID);
                             routeLinks.add(stop.getLinkId());
+                            String linkSeq = this.linkToVisumSequence.get(newLinkID);
                         }
                         postlength = tpi.length;
                         fromStop = stop;
+                        prevLinkSeq = tpi.linkSequence;
                     }
                     routeLinks.remove(routeLinks.size() - 1);
                     NetworkRoute netRoute = RouteUtils.createLinkNetworkRouteImpl(startLink, endLink);
@@ -178,6 +188,19 @@ public class TimeProfileExporter {
             });
         }
         log.info("Loading transit routes finished");
+    }
+
+    public void writeLinkSequence(String outputfolder) {
+        try (CSVWriter writer = new CSVWriter("", new String[]{"matsim_link", "link_sequence_visum"},
+                outputfolder + "/link_sequences.csv")) {
+            for(Id<Link> linkId: this.linkToVisumSequence.keySet()) {
+                writer.set("matsim_link", linkId.toString());
+                writer.set("link_sequence_visum", this.linkToVisumSequence.get(linkId));
+                writer.writeRow();
+            }
+        } catch (IOException e) {
+            log.error("Could not write file. " + e.getMessage(), e);
+        }
     }
 
     private Link createLink(Id<Link> linkId, Node fromNode, Node toNode, String mode, double length)    {
@@ -231,14 +254,24 @@ public class TimeProfileExporter {
         Visum.ComObject timeProfileItems = visum.getNetObject("TimeProfileItems");
         int nrOfTimeProfileItems = timeProfileItems.countActive();
         String[][] timeProfileItemAttributes = Visum.getArrayFromAttributeList(nrOfTimeProfileItems, timeProfileItems,
-                "TimeProfile\\ID", "Index", "LineRouteItem\\StopPointNo", "Dep", "Arr", "PostLength");
+                "TimeProfile\\ID", "Index", "LineRouteItem\\StopPointNo", "Dep", "Arr", "PostLength",
+                "Concatenate:UsedLineRouteItems\\OutLink\\No");
         for (int tpi = 0; tpi < nrOfTimeProfileItems; tpi++) {
+            String[] linkSeq = timeProfileItemAttributes[tpi][6].split(",");
+            List<String> linkSeqList = new ArrayList<>();
+            linkSeqList.add(linkSeq[0]);
+            for(int i = 1; i < linkSeq.length; i++)    {
+                if(!linkSeq[i].equals(linkSeq[i-1]))   {
+                    linkSeqList.add(linkSeq[i]);
+                }
+            }
             TimeProfile tp = timeProfileMap.get((int) Double.parseDouble(timeProfileItemAttributes[tpi][0]));
             tp.addTimeProfileItem(new TimeProfileItem((int) Double.parseDouble(timeProfileItemAttributes[tpi][1]),
                     (int) Double.parseDouble(timeProfileItemAttributes[tpi][2]),
                     Double.parseDouble(timeProfileItemAttributes[tpi][3]),
                     Double.parseDouble(timeProfileItemAttributes[tpi][4]),
-                    Double.parseDouble(timeProfileItemAttributes[tpi][5])));
+                    Double.parseDouble(timeProfileItemAttributes[tpi][5]),
+                    String.join(", ", linkSeqList)));
         }
 
         return timeProfileMap;
@@ -308,13 +341,15 @@ public class TimeProfileExporter {
         final double dep;
         final double arr;
         final double length;
+        final String linkSequence;
 
-        public TimeProfileItem(int index, int stopPoint, double dep, double arr, double length) {
+        public TimeProfileItem(int index, int stopPoint, double dep, double arr, double length, String linkSequence) {
             this.index = index;
             this.stopPoint = stopPoint;
             this.dep = dep;
             this.arr = arr;
             this.length = length;
+            this.linkSequence = linkSequence;
         }
     }
 }
