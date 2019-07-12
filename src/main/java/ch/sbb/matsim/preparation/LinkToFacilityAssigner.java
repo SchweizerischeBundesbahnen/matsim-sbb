5ef6b0a46c2dc756cec6b0c8ec0916e64d05c4ec
Denis Metrailler <denis.metrailler@sbb.ch>
2019-07-12 10:55:12 +0200

    filterNetwork has is own class

--- src/main/java/ch/sbb/matsim/preparation/LinkToFacilityAssigner.java
+++ src/main/java/ch/sbb/matsim/preparation/LinkToFacilityAssigner.java
@@ -15,16 +15,16 @@ import org.matsim.facilities.ActivityFacilities;
 import org.matsim.facilities.FacilitiesUtils;
 import org.matsim.facilities.FacilitiesWriter;
 import org.matsim.facilities.MatsimFacilitiesReader;
+import sun.nio.ch.Net;
 
 import java.util.Collections;
 
 public class LinkToFacilityAssigner {
 
     private ActivityFacilities facilities;
-    private Network filteredNetwork;
-    private NetworkFactory networkFactory;
 
-    public static void main(String[] args)  {
+
+    public static void main(String[] args) {
         String facilityFile = args[0];
         String networkFile = args[1];
         String outputFile = args[2];
@@ -38,62 +38,27 @@ public class LinkToFacilityAssigner {
 
     public void run(String facilityFile, String networkFile, String output) {
         readFacilities(facilityFile);
-        readAndFilterNetwork(networkFile);
-        assignLinkToFacility();
+        Network filteredNetwork = new FilteredNetwork().readAndFilterNetwork(networkFile);
+        assignLinkToFacility(filteredNetwork);
         writeFacilityFile(output);
     }
 
-    private void readFacilities(String facilityFile)   {
+    private void readFacilities(String facilityFile) {
         Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
         new MatsimFacilitiesReader(scenario).readFile(facilityFile);
         this.facilities = scenario.getActivityFacilities();
     }
 
-    private void readAndFilterNetwork(String networkFile) {
-        Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
-        new MatsimNetworkReader(scenario.getNetwork()).readFile(networkFile);
 
-        final Network carNetwork = NetworkUtils.createNetwork();
-        new TransportModeNetworkFilter(scenario.getNetwork()).filter(carNetwork, Collections.singleton(TransportMode.car));
-
-        this.filteredNetwork = NetworkUtils.createNetwork();
-        this.networkFactory = this.filteredNetwork.getFactory();
-
-        carNetwork.getLinks().values().stream().
-                filter(l -> l.getAttributes().getAttribute("accessControlled").toString().equals("0")).
-                forEach(this::addLinkToNetwork);
-    }
-
-    private void addLinkToNetwork(Link link)  {
-        Node origFromNode = link.getFromNode();
-        Node fromNode = addNodeIfNotExists(origFromNode);
-        Node origToNode = link.getToNode();
-        Node toNode = addNodeIfNotExists(origToNode);
-        Link xy2lLink = this.networkFactory.createLink(link.getId(), fromNode, toNode);
-        xy2lLink.setAllowedModes(link.getAllowedModes());
-        xy2lLink.setCapacity(link.getCapacity());
-        xy2lLink.setFreespeed(link.getFreespeed());
-        xy2lLink.setLength(link.getLength());
-        xy2lLink.setNumberOfLanes(link.getNumberOfLanes());
-        this.filteredNetwork.addLink(xy2lLink);
-    }
-
-    private Node addNodeIfNotExists(Node node)  {
-        Node newNode = this.filteredNetwork.getNodes().get(node.getId());
-        if (newNode == null) {
-            newNode = this.networkFactory.createNode(node.getId(), node.getCoord());
-            this.filteredNetwork.addNode(newNode);
-        }
-        return newNode;
-    }
-
-    private void assignLinkToFacility()  {
+    private void assignLinkToFacility(Network network) {
         this.facilities.getFacilities().values().
-                forEach(f -> FacilitiesUtils.setLinkID(f, NetworkUtils.getNearestLink(this.filteredNetwork,
+                forEach(f -> FacilitiesUtils.setLinkID(f, NetworkUtils.getNearestLink(network,
                         f.getCoord()).getId()));
     }
 
-    private void writeFacilityFile(String output)    {
+    private void writeFacilityFile(String output) {
         new FacilitiesWriter(this.facilities).write(output);
     }
 }
+
+
