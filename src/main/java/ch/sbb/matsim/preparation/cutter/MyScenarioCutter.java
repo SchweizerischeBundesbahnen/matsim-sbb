9c58a027975c37c790671841277c9586d5e2b7b2
u229187 <u229187@sbb.ch>
2019-08-09 15:21:36 +0200

    add a run method for easier testing

---
+++ src/main/java/ch/sbb/matsim/preparation/cutter/MyScenarioCutter.java
@@ -0,0 +1,18 @@
+package ch.sbb.matsim.preparation.cutter;
+
+import ch.sbb.matsim.zones.ZonesLoader;
+
+import java.io.IOException;
+
+public class MyScenarioCutter {
+    public static void main(String[] args) throws IOException {
+        CutExtent inside = new ShapeExtent(ZonesLoader.loadZones
+                ("id", "\\\\k13536\\mobi\\40_Projekte\\20190805_ScenarioCutter\\zones\\thun\\thun-agglo.shp", "ID"));
+        CutExtent outside = new ShapeExtent(ZonesLoader.loadZones
+                ("id", "\\\\k13536\\mobi\\40_Projekte\\20190805_ScenarioCutter\\zones\\thun\\thun-umgebung.shp", "ID"));
+
+        ScenarioCutter.run("C:\\devsbb\\data\\CH.10pct.2016", "CH.10pct.2016", "C:\\devsbb\\data\\CH2016_thun_cut", 1.0, false, inside, outside);
+
+    }
+
+}
