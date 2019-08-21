a8b050f8361d4acc6017c31574157b4881940315
u225744 <u225744@sbb.ch>
2019-08-21 08:50:41 +0200

    satisfy further requirements

--- src/main/java/ch/sbb/matsim/analysis/miv/ZoneBasedSpinne.java
+++ src/main/java/ch/sbb/matsim/analysis/miv/ZoneBasedSpinne.java
@@ -1,5 +1,6 @@
 package ch.sbb.matsim.analysis.miv;
 
+import ch.sbb.matsim.config.variables.Variables;
 import ch.sbb.matsim.csv.CSVWriter;
 import ch.sbb.matsim.zones.Zone;
 import ch.sbb.matsim.zones.Zones;
@@ -19,6 +20,7 @@ import org.matsim.core.population.routes.NetworkRoute;
 import org.matsim.core.router.TripStructureUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.io.UncheckedIOException;
+import org.matsim.utils.objectattributes.ObjectAttributesXmlReader;
 
 import java.io.IOException;
 import java.util.*;
@@ -27,40 +29,49 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ZoneBasedSpinne {
 
     private static final Logger log = Logger.getLogger(ZoneBasedSpinne.class);
-    private ConcurrentHashMap<Id<Link>, Integer> linkVolumes;
+    private ConcurrentHashMap<Id<Link>, LinkVolumes> linkVolumes = new ConcurrentHashMap<>();
 
     public static void main(String[] args)  {
         new ZoneBasedSpinne().run("\\\\k13536\\mobi\\50_Ergebnisse\\MOBi_2.0\\sim\\",
                 "\\\\k13536\\mobi\\50_Ergebnisse\\MOBi_2.0\\sim\\2.0.0_10pct_release\\output\\CH.10pct.2016.output_network.xml.gz",
                 "\\\\k13536\\mobi\\50_Ergebnisse\\MOBi_2.0\\zones\\v6\\output\\epsg_21781\\mobi_zones.shp",
-                "msrid", "1",
-                "\\\\k13536\\mobi\\99_Playgrounds\\PM\\Test_ZoneBasedMIVSpinne\\MIV_Spinne_MSR_Zuerich.csv.gz");
+                args[0], args[1], args[2], args[3]);
     }
 
     public void run(String plans, String networkFile, String shapeFile, String attName,
-                    String attValue, String csvOut)    {
+                    String attValue, String name, String csvOut)    {
         Zones zones = ZonesLoader.loadZones("spinne", shapeFile, null);
         ZonesQueryCache zonesCache = new ZonesQueryCache(zones);
 
-        Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
-        new MatsimNetworkReader(scenario.getNetwork()).readFile(networkFile);
+        HashSet<Id<Link>> linksInZone = getLinksInZone(networkFile, zonesCache, attName, attValue);
 
-        HashSet<Id<Link>> linksInZone = getLinksInZone(scenario, zonesCache, attName, attValue);
-
-        this.linkVolumes = new ConcurrentHashMap<>();
-        List<String> planFiles = new ArrayList<>();
+        List<InputFiles> planFiles = new ArrayList<>();
         for(int i = 1; i < 5; i++) {
-            planFiles.add(plans + "2.0.1."+i+"_release_25pct_"+i+"\\output\\CH.25pct."+i+".2016.output_plans.xml.gz");
+            planFiles.add(new InputFiles(plans + "2.0.1."+i+"_release_25pct_"+i+"\\output\\CH.25pct."+i+".2016.output_plans.xml.gz",
+                    plans + "2.0.1."+i+"_release_25pct_"+i+"\\output\\CH.25pct."+i+".2016.output_personAttributes.xml.gz"));
         }
         planFiles.parallelStream().forEach(planFile -> readPopulationAndCalcVolumes(planFile, linksInZone));
 
-        writeCSV(csvOut);
+        writeCSV(name, csvOut);
     }
 
-    private static HashSet<Id<Link>> getLinksInZone(Scenario scenario, ZonesQueryCache zonesCache, String attName,
+    private static class InputFiles {
+        private String plans;
+        private String personAttributes;
+
+        private InputFiles(String plans, String personAttributes)   {
+            this.plans = plans;
+            this.personAttributes = personAttributes;
+        }
+    }
+
+    private static HashSet<Id<Link>> getLinksInZone(String networkFile, ZonesQueryCache zonesCache, String attName,
                                                     String attValue)  {
+        Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        new MatsimNetworkReader(scenario.getNetwork()).readFile(networkFile);
+
         HashSet<Id<Link>> linksInZone = new HashSet<>();
-        scenario.getNetwork().getLinks().values().parallelStream().
+        scenario.getNetwork().getLinks().values().stream().
                 filter(link -> link.getAllowedModes().contains(TransportMode.car)).
                 forEach(link -> {
                     Coord fromCoord = link.getFromNode().getCoord();
@@ -69,10 +80,7 @@ public class ZoneBasedSpinne {
                     double y = (fromCoord.getY() + toCoord.getY()) / 2.0;
                     Zone z = zonesCache.findZone(x, y);
                     String shapeValue = z == null ? null : z.getAttribute(attName).toString();
-                    if (shapeValue == null) {
-                        return;
-                    }
-                    if (shapeValue.equals(attValue)) {
+                    if (shapeValue != null && shapeValue.equals(attValue)) {
                         linksInZone.add(link.getId());
                     }
                 });
@@ -81,46 +89,75 @@ public class ZoneBasedSpinne {
         return linksInZone;
     }
 
-    private void readPopulationAndCalcVolumes(String plansFile, HashSet<Id<Link>> linksInZone)   {
-        log.info("loading plans from file " + plansFile);
+    private void readPopulationAndCalcVolumes(InputFiles files, HashSet<Id<Link>> linksInZone)   {
+        log.info("loading plans from file " + files.plans);
 
         Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
-        StreamingPopulationReader r = new StreamingPopulationReader(scenario);
+        // TODO: get rid of personAttributes file
+        new ObjectAttributesXmlReader(scenario.getPopulation().getPersonAttributes()).readFile(files.personAttributes);
+
+        Scenario scenario1 = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        StreamingPopulationReader r = new StreamingPopulationReader(scenario1);
         r.addAlgorithm(person -> {
-            Plan selectedPlan = person.getSelectedPlan();
-            TripStructureUtils.getLegs(selectedPlan).parallelStream().
-                    filter(leg -> leg.getMode().equals(TransportMode.car)).
-                    forEach(leg -> {
-                        NetworkRoute route = (NetworkRoute) leg.getRoute();
-                        if (linksInZone.contains(route.getStartLinkId()) || linksInZone.contains(route.getEndLinkId())) {
-                            addRouteToLinkVolumes(route);
-                        }
-                        else    {
-                            for (Id<Link> link: route.getLinkIds()) {
-                                if (linksInZone.contains(link)) {
-                                    addRouteToLinkVolumes(route);
-                                    break;
+            String subpop = scenario.getPopulation().getPersonAttributes().getAttribute(person.getId().toString(),
+                    Variables.SUBPOPULATION).toString();
+            // take agent if it is not a freight agent
+            if(subpop.equals("regular") || subpop.equals("cb_road") || subpop.equals("airport_road"))    {
+                Plan selectedPlan = person.getSelectedPlan();
+                TripStructureUtils.getLegs(selectedPlan).parallelStream().
+                        filter(leg -> (leg.getMode().equals(TransportMode.car) || leg.getMode().equals(TransportMode.ride))).
+                        forEach(leg -> {
+                            NetworkRoute route = (NetworkRoute) leg.getRoute();
+                            if (linksInZone.contains(route.getStartLinkId()) || linksInZone.contains(route.getEndLinkId())) {
+                                addRouteToOrigDestVolumes(route);
+                            } else {
+                                for (Id<Link> link : route.getLinkIds()) {
+                                    if (linksInZone.contains(link)) {
+                                        addRouteToTransitVolumes(route);
+                                        break;
+                                    }
                                 }
                             }
-                        }
-                    });
+                        });
+            }
         });
-        r.readFile(plansFile);
+        r.readFile(files.plans);
     }
 
-    private void addRouteToLinkVolumes(NetworkRoute route)  {
-        this.linkVolumes.merge(route.getStartLinkId(), 1, (a, b) -> a + b);
-        this.linkVolumes.merge(route.getEndLinkId(), 1, (a, b) -> a + b);
-        route.getLinkIds().forEach(linkId -> this.linkVolumes.merge(linkId, 1, (a, b) -> a + b));
+    private void addRouteToOrigDestVolumes(NetworkRoute route)  {
+        this.putIfAbsent(route);
+        this.linkVolumes.get(route.getStartLinkId()).incVolOrigDest();
+        this.linkVolumes.get(route.getEndLinkId()).incVolOrigDest();
+        route.getLinkIds().forEach(linkId -> this.linkVolumes.get(linkId).incVolOrigDest());
     }
 
-    private void writeCSV(String fileName) {
-        try (CSVWriter writer = new CSVWriter("", VOLUMES_COLUMNS, fileName)) {
-            for (Map.Entry<Id<Link>, Integer> entry : this.linkVolumes.entrySet()) {
+    private void addRouteToTransitVolumes(NetworkRoute route)  {
+        this.putIfAbsent(route);
+        this.linkVolumes.get(route.getStartLinkId()).incVolTransit();
+        this.linkVolumes.get(route.getEndLinkId()).incVolTransit();
+        route.getLinkIds().forEach(linkId -> this.linkVolumes.get(linkId).incVolTransit());
+    }
+
+    private void putIfAbsent(NetworkRoute route) {
+        this.linkVolumes.putIfAbsent(route.getStartLinkId(), new LinkVolumes());
+        this.linkVolumes.putIfAbsent(route.getEndLinkId(), new LinkVolumes());
+        route.getLinkIds().forEach(linkId -> this.linkVolumes.putIfAbsent(linkId, new LinkVolumes()));
+    }
+
+    private void writeCSV(String name, String fileName) {
+        String[] columesColumns = new String[]{
+                "LINK_ID_SIM",
+                "Spinne_"+ name +"_OrigDest",
+                "Spinne_"+ name +"_Transit"
+        };
+
+        try (CSVWriter writer = new CSVWriter("", columesColumns, fileName)) {
+            for (Map.Entry<Id<Link>, LinkVolumes> entry : this.linkVolumes.entrySet()) {
                 Id<Link> link = entry.getKey();
-                int volume = entry.getValue();
-                writer.set(VOLUMES_COLUMNS[0], link.toString());
-                writer.set(VOLUMES_COLUMNS[1], Integer.toString(volume));
+                LinkVolumes volume = entry.getValue();
+                writer.set(columesColumns[0], link.toString());
+                writer.set(columesColumns[1], Integer.toString(volume.volumesOrigDest));
+                writer.set(columesColumns[2], Integer.toString(volume.volumesTransit));
                 writer.writeRow();
             }
         } catch (IOException e) {
@@ -128,8 +165,16 @@ public class ZoneBasedSpinne {
         }
     }
 
-    private static final String[] VOLUMES_COLUMNS = new String[]{
-            "LINK_ID_SIM",
-            "VOLUME_SIM"
-    };
+    private static class LinkVolumes {
+        private int volumesOrigDest = 0;
+        private int volumesTransit = 0;
+
+        private void incVolOrigDest() {
+            this.volumesOrigDest ++;
+        }
+
+        private void incVolTransit() {
+            this.volumesTransit ++;
+        }
+    }
 }
