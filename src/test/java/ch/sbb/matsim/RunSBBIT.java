348f3f035f8973d6e3effcbee9fd4748964e640f
jbischoff <joschka.bischoff@sbb.ch>
2019-09-06 08:59:15 +0200

    add an Integration Test for RunSBB

---
+++ src/test/java/ch/sbb/matsim/RunSBBIT.java
@@ -0,0 +1,15 @@
+package ch.sbb.matsim;
+
+import org.junit.Test;
+
+/**
+ * An integration test to see if the Mobi Scenario is running.
+ */
+public class RunSBBIT {
+
+    @Test
+    public void main() {
+        RunSBB.main(new String[]{"test/input/scenarios/mobi20test/runSBBTestConfig.xml"});
+
+    }
+}
