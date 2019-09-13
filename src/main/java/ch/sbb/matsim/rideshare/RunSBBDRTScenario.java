ddaa09554e90d4e64f9aa0e06e4b5f32fbc0142a
jbischoff <joschka.bischoff@sbb.ch>
2019-09-13 14:46:43 +0200

    add drt scenario runner

---
+++ src/main/java/ch/sbb/matsim/rideshare/RunSBBDRTScenario.java
@@ -0,0 +1,64 @@
+package ch.sbb.matsim.rideshare;
+
+import ch.sbb.matsim.RunSBB;
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.drt.run.*;
+import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpModule;
+import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigGroup;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RunSBBDRTScenario {
+
+    private final static Logger log = Logger.getLogger(RunSBBDRTScenario.class);
+
+    public static void main(String[] args) {
+        System.setProperty("matsim.preferLocalDtds", "true");
+        final String configFile = args[0];
+        log.info(configFile);
+        Config config = ConfigUtils.loadConfig(configFile, getSBBAndDrtConfigGroups());
+        if (args.length > 1)
+            config.controler().setOutputDirectory(args[1]);
+
+        prepareDrtConfig(config);
+        Scenario scenario = DrtControlerCreator.createScenarioWithDrtRouteFactory(config);
+        ScenarioUtils.loadScenario(scenario);
+        RunSBB.addSBBDefaultScenarioModules(scenario);
+        Controler controler = new Controler(scenario);
+        RunSBB.addSBBDefaultControlerModules(controler);
+        prepareDrtControler(controler);
+        controler.run();
+    }
+
+    public static ConfigGroup[] getSBBAndDrtConfigGroups() {
+        List<ConfigGroup> configGroupList = new ArrayList<>();
+        configGroupList.addAll(Arrays.asList(RunSBB.sbbDefaultConfigGroups));
+        configGroupList.add(new DrtConfigGroup());
+        configGroupList.add(new DvrpConfigGroup());
+        return configGroupList.toArray(new ConfigGroup[configGroupList.size()]);
+    }
+
+    public static void prepareDrtConfig(Config config) {
+        DrtConfigGroup drtCfg = DrtConfigGroup.get(config);
+        DrtConfigs.adjustDrtConfig(drtCfg, config.planCalcScore());
+        config.addConfigConsistencyChecker(new DrtConfigConsistencyChecker());
+        config.checkConsistency();
+    }
+
+    public static void prepareDrtControler(Controler controler) {
+        controler.addOverridingModule(new DrtModule());
+        controler.addOverridingModule(new DvrpModule());
+        controler.configureQSimComponents(DvrpQSimComponents.activateModes(DrtConfigGroup.get(controler.getConfig()).getMode()));
+    }
+
+
+}
---
+++ src/main/java/ch/sbb/matsim/rideshare/utils/FleetGenerator.java
@@ -0,0 +1,59 @@
+package ch.sbb.matsim.rideshare.utils;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.contrib.dvrp.fleet.DvrpVehicle;
+import org.matsim.contrib.dvrp.fleet.DvrpVehicleSpecification;
+import org.matsim.contrib.dvrp.fleet.FleetWriter;
+import org.matsim.contrib.dvrp.fleet.ImmutableDvrpVehicleSpecification;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.gbl.MatsimRandom;
+import org.matsim.core.network.io.MatsimNetworkReader;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class FleetGenerator {
+    /**
+     * Adjust these variables and paths to your need.
+     */
+
+    private static final int numberOfVehicles = 100;
+    private static final int seatsPerVehicle = 2; //this is important for DRT, value is not used by taxi
+    private static final double operationStartTime = 0;
+    private static final double operationEndTime = 30 * 60 * 60; //24h
+    private static final Random random = MatsimRandom.getRandom();
+
+    private static final Path networkFile = Paths.get("\\\\k13536\\mobi\\40_Projekte\\20190913_Ridesharing\\scenarios\\0.01_neuenburg\\input\\network.xml.gz");
+    private static final Path outputFile = Paths.get("\\\\k13536\\mobi\\40_Projekte\\20190913_Ridesharing\\scenarios\\0.01_neuenburg\\input\\fleetVehicles.xml");
+
+    public static void main(String[] args) {
+
+        new FleetGenerator().run();
+    }
+
+    private void run() {
+
+        Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        new MatsimNetworkReader(scenario.getNetwork()).readFile(networkFile.toString());
+        final int[] i = {0};
+        Stream<DvrpVehicleSpecification> vehicleSpecificationStream = scenario.getNetwork().getLinks().entrySet().stream()
+                .filter(entry -> entry.getValue().getAllowedModes().contains(TransportMode.car)) // drt can only start on links with Transport mode 'car'
+
+                .sorted((e1, e2) -> (random.nextInt(2) - 1)) // shuffle links
+                .limit(numberOfVehicles) // select the first *numberOfVehicles* links
+                .map(entry -> ImmutableDvrpVehicleSpecification.newBuilder()
+                        .id(Id.create("drt_" + i[0]++, DvrpVehicle.class))
+                        .startLinkId(entry.getKey())
+                        .capacity(seatsPerVehicle)
+                        .serviceBeginTime(operationStartTime)
+                        .serviceEndTime(operationEndTime)
+                        .build());
+
+        new FleetWriter(vehicleSpecificationStream).write(outputFile.toString());
+    }
+}
