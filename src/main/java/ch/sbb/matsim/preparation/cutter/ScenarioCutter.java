ba3f98cc73ab4aa3e349b21ecd1ab7e0c7b74cc4
jbischoff <joschka.bischoff@sbb.ch>
2019-09-21 15:07:54 +0200

    minor

--- src/main/java/ch/sbb/matsim/preparation/cutter/ScenarioCutter.java
+++ src/main/java/ch/sbb/matsim/preparation/cutter/ScenarioCutter.java
@@ -24,6 +24,7 @@ import org.matsim.core.network.io.NetworkChangeEventsWriter;
 import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.population.routes.NetworkRoute;
 import org.matsim.core.population.routes.RouteUtils;
+import org.matsim.core.router.TripStructureUtils;
 import org.matsim.core.router.util.TravelTime;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.trafficmonitoring.FreeSpeedTravelTime;
@@ -754,16 +755,9 @@ public class ScenarioCutter {
     }
 
     private void removeRoutesWithLinksThatDoNotExist(Plan plan, CutContext ctx) {
-
-        plan.getPlanElements().stream()
-                .filter(Leg.class::isInstance)
-                .forEach(leg -> {
-                    if (!ctx.dest.getNetwork().getLinks().containsKey(((Leg) leg).getRoute().getStartLinkId()) || !ctx.dest.getNetwork().getLinks().containsKey(((Leg) leg).getRoute().getEndLinkId())) {
-                        ((Leg) leg).setRoute(null);
-                    }
-
-                });
-
+        TripStructureUtils.getLegs(plan).stream()
+                .filter(leg -> ctx.dest.getNetwork().getLinks().containsKey(leg.getRoute().getStartLinkId()) || !ctx.dest.getNetwork().getLinks().containsKey(leg.getRoute().getEndLinkId()))
+                .forEach(leg -> leg.setRoute(null));
     }
 
     private void removeDurationWhenBothAreSet(Plan plan) {
