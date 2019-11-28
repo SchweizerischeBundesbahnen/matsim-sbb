0743e501a23ff5a67d34db495d37fb56258f967d
Knupp Annette (P-V-APL-MT-MT2) <annette.knupp@sbb.ch>
2019-11-28 19:07:49 +0100

    Test anpassen an angepasste Parameter onNetwork

--- src/test/java/ch/sbb/matsim/config/SBBIntermodalConfigGroupTest.java
+++ src/test/java/ch/sbb/matsim/config/SBBIntermodalConfigGroupTest.java
@@ -24,7 +24,8 @@ public class SBBIntermodalConfigGroupTest {
         mode1a.setDetourFactor(0.95); // the bicycle takes short cuts ;-)
         mode1a.setWaitingTime(3*60);
         mode1a.setMUTT(-0.6);
-        mode1a.setOnNetwork(false);
+        mode1a.setSimulatedOnNetwork(false);
+        mode1a.setRoutedOnNetwork(false);
         intermodal1.addModeParameters(mode1a);
         SBBIntermodalConfigGroup.SBBIntermodalModeParameterSet mode1b = new SBBIntermodalConfigGroup.SBBIntermodalModeParameterSet();
         mode1b.setConstant(1.4);
@@ -32,7 +33,8 @@ public class SBBIntermodalConfigGroupTest {
         mode1b.setDetourFactor(1.08);
         mode1b.setWaitingTime(2*60);
         mode1b.setMUTT(-0.9);
-        mode1b.setOnNetwork(true);
+        mode1b.setSimulatedOnNetwork(true);
+        mode1b.setRoutedOnNetwork(true);
         intermodal1.addModeParameters(mode1b);
 
         Config config1 = ConfigUtils.createConfig(intermodal1);
@@ -72,13 +74,15 @@ public class SBBIntermodalConfigGroupTest {
         Assert.assertEquals(mode1a.getDetourFactor(), mode2a.getDetourFactor(), 1e-7);
         Assert.assertEquals(mode1a.getWaitingTime(), mode2a.getWaitingTime(), 1e-7);
         Assert.assertEquals(mode1a.getMUTT(), mode2a.getMUTT(), 1e-7);
-        Assert.assertEquals(mode1a.isOnNetwork(), mode2a.isOnNetwork());
+        Assert.assertEquals(mode1a.isSimulatedOnNetwork(), mode2a.isSimulatedOnNetwork());
+        Assert.assertEquals(mode1a.isRoutedOnNetwork(), mode2a.isRoutedOnNetwork());
 
         Assert.assertEquals(mode1b.getConstant(), mode2b.getConstant(), 1e-7);
         Assert.assertEquals(mode1b.getDetourFactor(), mode2b.getDetourFactor(), 1e-7);
         Assert.assertEquals(mode1b.getWaitingTime(), mode2b.getWaitingTime(), 1e-7);
         Assert.assertEquals(mode1b.getMUTT(), mode2b.getMUTT(), 1e-7);
-        Assert.assertEquals(mode1b.isOnNetwork(), mode2b.isOnNetwork());
+        Assert.assertEquals(mode1b.isSimulatedOnNetwork(), mode2b.isSimulatedOnNetwork());
+        Assert.assertEquals(mode1b.isRoutedOnNetwork(), mode2b.isRoutedOnNetwork());
     }
 
 }
