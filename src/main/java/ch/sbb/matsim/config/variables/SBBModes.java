6f04393a282da1743beba3db828001c81087e06b
jbischoff <joschka.bischoff@sbb.ch>
2019-12-12 15:01:07 +0100

    revert accidental merge mistake

--- src/main/java/ch/sbb/matsim/config/variables/SBBModes.java
+++ src/main/java/ch/sbb/matsim/config/variables/SBBModes.java
@@ -2,7 +2,9 @@ package ch.sbb.matsim.config.variables;
 
 import org.matsim.api.core.v01.TransportMode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SBBModes {
@@ -16,7 +18,8 @@ public class SBBModes {
     public static final String AVTAXI = "avtaxi";
     public static final String AVFEEDER = "avfeeder";
     public static final String DRT = TransportMode.drt;
-
+    public static final String PT_FALLBACK_MODE = TransportMode.transit_walk;
+    public static final String NON_NETWORK_WALK = TransportMode.non_network_walk;
 
     public static final int DEFAULT_MODE_HIERARCHY = 99;
     public final static Map<String, Integer> mode2HierarchalNumber;
@@ -29,5 +32,28 @@ public class SBBModes {
         mode2HierarchalNumber.put(RIDE, 20);
         mode2HierarchalNumber.put(BIKE, 30);
         mode2HierarchalNumber.put(WALK, 40);
+        mode2HierarchalNumber.put(PT_FALLBACK_MODE, 41);
+        mode2HierarchalNumber.put(NON_NETWORK_WALK, 50);
+    }
+
+    public static class PTSubModes {
+        public static final String RAIL = "rail";
+        public static final String TRAM = "tram";
+        public static final String BUS = "bus";
+        public static final String OTHER = "other"; // example for "other": Seilbahn, Gondelbahn, Schiff, ...
+        public final static List<String> subModeList;
+
+        static {
+            subModeList = new ArrayList<>();
+            subModeList.add(RAIL);
+            subModeList.add(TRAM);
+            subModeList.add(BUS);
+            subModeList.add(OTHER);
+            // TODO: remove detPt as soon as we merged the pt-submodes
+            subModeList.add("detPt");
+        }
+
+        private PTSubModes() {
+        }
     }
 }
