277355873bd7810c21687ae958b33ad2104ead77
u229187 <u229187@sbb.ch>
2020-06-08 15:36:46 +0200

    use TransitRoutesStopBuilder, experimental transit rotues

--- src/test/java/ch/sbb/matsim/scoring/TransferScoringTest.java
+++ src/test/java/ch/sbb/matsim/scoring/TransferScoringTest.java
@@ -1,6 +1,8 @@
 package ch.sbb.matsim.scoring;
 
 import ch.sbb.matsim.config.SBBBehaviorGroupsConfigGroup;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Rule;
@@ -13,7 +15,12 @@ import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.NetworkFactory;
 import org.matsim.api.core.v01.network.Node;
-import org.matsim.api.core.v01.population.*;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.PopulationFactory;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
@@ -22,15 +29,21 @@ import org.matsim.core.population.routes.NetworkRoute;
 import org.matsim.core.population.routes.RouteUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.collections.CollectionUtils;
-import org.matsim.core.utils.misc.Time;
 import org.matsim.pt.PtConstants;
-import org.matsim.pt.routes.ExperimentalTransitRoute;
-import org.matsim.pt.transitSchedule.api.*;
+import org.matsim.pt.routes.DefaultTransitPassengerRoute;
+import org.matsim.pt.transitSchedule.api.Departure;
+import org.matsim.pt.transitSchedule.api.TransitLine;
+import org.matsim.pt.transitSchedule.api.TransitRoute;
+import org.matsim.pt.transitSchedule.api.TransitRouteStop;
+import org.matsim.pt.transitSchedule.api.TransitSchedule;
+import org.matsim.pt.transitSchedule.api.TransitScheduleFactory;
+import org.matsim.pt.transitSchedule.api.TransitStopFacility;
 import org.matsim.testcases.MatsimTestUtils;
-import org.matsim.vehicles.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.VehicleCapacity;
+import org.matsim.vehicles.VehicleType;
+import org.matsim.vehicles.Vehicles;
+import org.matsim.vehicles.VehiclesFactory;
 
 /**
  * Main idea of the test: Run a mini-scenario with a single agent twice, once with default MATSim scoring, once with SBB Scoring.
@@ -201,10 +214,10 @@ public class TransferScoringTest {
             TransitLine blueLine = sf.createTransitLine(Id.create("blue", TransitLine.class));
             NetworkRoute blueNetRoute = RouteUtils.createLinkNetworkRouteImpl(Id.create(1, Link.class), Id.create(2, Link.class));
             List<TransitRouteStop> blueStops = new ArrayList<>();
-            blueStops.add(sf.createTransitRouteStop(stop1, Time.getUndefinedTime(), 0.0));
-            blueStops.add(sf.createTransitRouteStop(stop2, 120.0, Time.getUndefinedTime()));
+            blueStops.add(sf.createTransitRouteStopBuilder(stop1).departureOffset(0.).build());
+            blueStops.add(sf.createTransitRouteStopBuilder(stop2).arrivalOffset(120.0).build());
             TransitRoute blueRoute = sf.createTransitRoute(Id.create("blue1", TransitRoute.class), blueNetRoute, blueStops, "train");
-            Departure blueDeparture = sf.createDeparture(Id.create(1, Departure.class), 8*3600);
+            Departure blueDeparture = sf.createDeparture(Id.create(1, Departure.class), 8 * 3600);
             blueDeparture.setVehicleId(Id.create("b1", Vehicle.class));
             blueRoute.addDeparture(blueDeparture);
 
@@ -214,10 +227,10 @@ public class TransferScoringTest {
             TransitLine redLine = sf.createTransitLine(Id.create("red", TransitLine.class));
             NetworkRoute redNetRoute = RouteUtils.createLinkNetworkRouteImpl(Id.create(2, Link.class), Id.create(3, Link.class));
             List<TransitRouteStop> redStops = new ArrayList<>();
-            redStops.add(sf.createTransitRouteStop(stop2, Time.getUndefinedTime(), 0.0));
-            redStops.add(sf.createTransitRouteStop(stop3, 120.0, Time.getUndefinedTime()));
+            redStops.add(sf.createTransitRouteStopBuilder(stop2).departureOffset(0.).build());
+            redStops.add(sf.createTransitRouteStopBuilder(stop3).arrivalOffset(120.).build());
             TransitRoute redRoute = sf.createTransitRoute(Id.create("red1", TransitRoute.class), redNetRoute, redStops, "train");
-            Departure redDeparture = sf.createDeparture(Id.create(1, Departure.class), 8*3600 + 240);
+            Departure redDeparture = sf.createDeparture(Id.create(1, Departure.class), 8 * 3600 + 240);
             redDeparture.setVehicleId(Id.create("r1", Vehicle.class));
             redRoute.addDeparture(redDeparture);
             redLine.addRoute(redRoute);
@@ -270,11 +283,11 @@ public class TransferScoringTest {
             plan.addLeg(accessLeg);
             plan.addActivity(ptAct1);
             Leg pt1Leg = pf.createLeg(TransportMode.pt);
-            pt1Leg.setRoute(new ExperimentalTransitRoute(stop1, blueLine, blueRoute, stop2));
+            pt1Leg.setRoute(new DefaultTransitPassengerRoute(stop1, blueLine, blueRoute, stop2));
             plan.addLeg(pt1Leg);
             plan.addActivity(transferAct);
             Leg pt2Leg = pf.createLeg(TransportMode.pt);
-            pt2Leg.setRoute(new ExperimentalTransitRoute(stop2, redLine, redRoute, stop3));
+            pt2Leg.setRoute(new DefaultTransitPassengerRoute(stop2, redLine, redRoute, stop3));
             plan.addLeg(pt2Leg);
             plan.addActivity(ptAct2);
             Leg egressLeg = pf.createLeg(TransportMode.non_network_walk);
