32c40689af43d2b01f8481833806999e7a163ec7
u229187 <joschka.bischoff@sbb.ch>
2021-01-22 09:48:50 +0100

    use / instead of \; remove manual overwrite of outputdir

--- src/test/java/ch/sbb/matsim/RunSBBIT.java
+++ src/test/java/ch/sbb/matsim/RunSBBIT.java
@@ -13,7 +13,6 @@ public class RunSBBIT {
 	public void runSBBIt() {
 		System.setProperty("matsim.preferLocalDtds", "true");
 		Config config = RunSBB.buildConfig("test/input/scenarios/mobi31test/config.xml");
-		config.controler().setOutputDirectory("test/output/intermodaltest");
 		config.controler().setCreateGraphs(true);
 		RunSBB.run(config);
 	}
