a0d4770fc8c84129119cc437cf9f63a635bb228e
u229187 <joschka.bischoff@sbb.ch>
2021-02-18 15:58:51 +0100

    add RailDemandReporting

---
+++ src/main/java/ch/sbb/matsim/analysis/tripsandlegsanalysis/RailDemandReporting.java
@@ -0,0 +1,230 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2020 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package ch.sbb.matsim.analysis.tripsandlegsanalysis;
+
+import ch.sbb.matsim.config.variables.Variables;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import javax.inject.Inject;
+import org.apache.commons.lang3.mutable.MutableDouble;
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.io.MatsimNetworkReader;
+import org.matsim.core.population.io.PopulationReader;
+import org.matsim.core.router.TripStructureUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.scoring.ExperiencedPlansService;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.pt.routes.DefaultTransitPassengerRoute;
+import org.matsim.pt.transitSchedule.api.TransitLine;
+import org.matsim.pt.transitSchedule.api.TransitSchedule;
+import org.matsim.pt.transitSchedule.api.TransitScheduleReader;
+
+public class RailDemandReporting {
+
+    public static final String NONE = "none";
+    final Logger logger = Logger.getLogger(getClass());
+    private final RailTripsAnalyzer railTripsAnalyzer;
+    private final Map<String, MutableDouble> pkmSparte = new TreeMap<>();
+    private final Map<String, MutableDouble> pkmLfpCat = new TreeMap<>();
+    private final Map<String, MutableDouble> pkmAbgrenzung = new TreeMap<>();
+
+    private final Map<Id<TransitLine>, String> lineSparte = new HashMap<>();
+    private final Map<Id<TransitLine>, String> lineLfpCat = new HashMap<>();
+    private final Map<Id<TransitLine>, String> lineAbgrenzung = new HashMap<>();
+    private final double scaleFactor;
+    @Inject
+    private ExperiencedPlansService experiencedPlansService;
+    private double fqdistance = 0;
+    private int fqtrips = 0;
+    private double railDistance = 0.0;
+    private int railtrips = 0;
+
+    @Inject
+    public RailDemandReporting(RailTripsAnalyzer railTripsAnalyzer, TransitSchedule schedule, double scaleFactor) {
+        this.railTripsAnalyzer = railTripsAnalyzer;
+        prepareCategories(schedule);
+        this.scaleFactor = scaleFactor;
+    }
+
+    public static void main(String[] args) {
+        String experiencedPlansFile = args[0];
+        String transitScheduleFile = args[1];
+        String networkFile = args[2];
+        double scaleFactor = Double.parseDouble(args[3]);
+        String outputFile = args[4];
+
+        Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        new TransitScheduleReader(scenario).readFile(transitScheduleFile);
+        new MatsimNetworkReader(scenario.getNetwork()).readFile(networkFile);
+        new PopulationReader(scenario).readFile(experiencedPlansFile);
+        RailTripsAnalyzer railTripsAnalyzer = new RailTripsAnalyzer(scenario.getTransitSchedule(), scenario.getNetwork());
+        RailDemandReporting railDemandReporting = new RailDemandReporting(railTripsAnalyzer, scenario.getTransitSchedule(), scaleFactor);
+        railDemandReporting.calcAndWriteDistanceReporting(outputFile, scenario.getPopulation().getPersons().values().stream().map(p -> p.getSelectedPlan()).collect(Collectors.toSet()));
+    }
+
+    private void reset() {
+        pkmSparte.values().forEach(d -> d.setValue(0));
+        pkmAbgrenzung.values().forEach(d -> d.setValue(0));
+        pkmLfpCat.values().forEach(d -> d.setValue(0));
+        fqdistance = .0;
+        fqtrips = 0;
+        railtrips = 0;
+        railDistance = 0.0;
+    }
+
+    private void calcAndWriteDistanceReporting(String outputfile, Collection<Plan> plans) {
+        reset();
+        aggregateRailDistances(plans);
+        writeRailDistanceReporting(outputfile);
+    }
+
+    public void calcAndwriteIterationDistanceReporting(String outputfile) {
+        calcAndWriteDistanceReporting(outputfile, experiencedPlansService.getExperiencedPlans().values());
+    }
+
+    private void writeRailDistanceReporting(String outputfile) {
+        try (BufferedWriter bw = IOUtils.getBufferedWriter(outputfile)) {
+            bw.write("SIMBA MOBi Rail Reporting");
+            bw.newLine();
+            bw.newLine();
+            bw.write("Gesamt Bahn");
+            bw.newLine();
+            bw.write("PF;" + railtrips * scaleFactor);
+            bw.newLine();
+            bw.write("PKM;" + (int) Math.round(scaleFactor * railDistance / 1000.));
+            bw.newLine();
+            bw.newLine();
+            bw.write("FQ-relevante Werte");
+            bw.newLine();
+            bw.write("PF;" + fqtrips * scaleFactor);
+            bw.newLine();
+            bw.write("PKM;" + (int) Math.round(scaleFactor * fqdistance / 1000.));
+            bw.newLine();
+            bw.newLine();
+            bw.write("PKM je Sparte");
+            for (Entry<String, MutableDouble> e : pkmSparte.entrySet()) {
+                bw.newLine();
+                bw.write(e.getKey() + ";" + Math.round(scaleFactor * e.getValue().doubleValue()) / 1000);
+            }
+            bw.newLine();
+            bw.newLine();
+            bw.write("PKM je Abgrenzgrupe");
+            for (Entry<String, MutableDouble> e : pkmAbgrenzung.entrySet()) {
+                bw.newLine();
+                bw.write(e.getKey() + ";" + Math.round(scaleFactor * e.getValue().doubleValue()) / 1000);
+            }
+
+            bw.newLine();
+            bw.newLine();
+            bw.write("PKM je LFP-Betreiberkategorie");
+            for (Entry<String, MutableDouble> e : pkmLfpCat.entrySet()) {
+                bw.newLine();
+                bw.write(e.getKey() + ";" + Math.round(scaleFactor * e.getValue().doubleValue()) / 1000);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void prepareCategories(TransitSchedule schedule) {
+
+        for (TransitLine line : schedule.getTransitLines().values()) {
+            if (railTripsAnalyzer.isRailLine(line.getId())) {
+                var route = line.getRoutes().values().stream().findFirst().get();
+                String sparte = (String) route.getAttributes().getAttribute(Variables.SPARTE);
+                if (sparte == null) {
+                    logger.warn("No " + Variables.SPARTE + " defined for transit line " + line.getId());
+                    sparte = NONE;
+                }
+                lineSparte.put(line.getId(), sparte);
+                pkmSparte.computeIfAbsent(sparte, (a) -> new MutableDouble(0));
+
+                String abgrenzgruppe = (String) route.getAttributes().getAttribute(Variables.ABGRENZGRUPPE);
+                if (abgrenzgruppe == null) {
+                    logger.warn("No " + Variables.ABGRENZGRUPPE + " defined for transit line " + line.getId());
+                    abgrenzgruppe = NONE;
+                }
+                lineAbgrenzung.put(line.getId(), abgrenzgruppe);
+                pkmAbgrenzung.computeIfAbsent(abgrenzgruppe, (a) -> new MutableDouble(0));
+
+                String lfpCat = (String) route.getAttributes().getAttribute(Variables.BETREIBERAGGRLFP);
+                if (lfpCat == null) {
+                    logger.warn("No " + Variables.BETREIBERAGGRLFP + " defined for transit line " + line.getId());
+                    lfpCat = NONE;
+                }
+                lineLfpCat.put(line.getId(), lfpCat);
+                pkmLfpCat.computeIfAbsent(lfpCat, (a) -> new MutableDouble(0));
+
+            }
+        }
+    }
+
+    private void aggregateRailDistances(Collection<Plan> plans) {
+        Set<DefaultTransitPassengerRoute> allRailRoutes = plans
+                .stream()
+                .flatMap(plan -> TripStructureUtils.getLegs(plan).stream())
+                .filter(leg -> leg.getRoute().getRouteType().equals(DefaultTransitPassengerRoute.ROUTE_TYPE))
+                .map(leg -> (DefaultTransitPassengerRoute) leg.getRoute())
+                .filter(defaultTransitPassengerRoute -> railTripsAnalyzer.isRailLine(defaultTransitPassengerRoute.getLineId()))
+                .collect(Collectors.toSet());
+
+        for (var route : allRailRoutes) {
+            double distance = railTripsAnalyzer.getDomesticRailDistance_m(route);
+            if (distance > 0.0) {
+                var transitLineId = route.getLineId();
+                pkmLfpCat.get(lineLfpCat.get(transitLineId)).add(distance);
+                pkmAbgrenzung.get(lineAbgrenzung.get(transitLineId)).add(distance);
+                pkmSparte.get(lineSparte.get(transitLineId)).add(distance);
+            }
+        }
+        aggregateFQValues(plans);
+
+    }
+
+    private void aggregateFQValues(Collection<Plan> plans) {
+        plans
+                .stream()
+                .flatMap(plan -> TripStructureUtils.getTrips(plan).stream())
+                .forEach(trip -> {
+                    double tripFQDistance = railTripsAnalyzer.getFQDistance(trip);
+                    if (tripFQDistance > 0) {
+                        this.fqdistance += tripFQDistance;
+                        this.fqtrips++;
+                    }
+                    final double tripRailDistance = railTripsAnalyzer.calcRailDistance(trip);
+                    this.railDistance += tripRailDistance;
+                    if (tripRailDistance > 0) {
+                        this.railtrips++;
+                    }
+                });
+    }
+
+}
--- src/main/java/ch/sbb/matsim/analysis/tripsandlegsanalysis/RailTripsAnalyzer.java
+++ src/main/java/ch/sbb/matsim/analysis/tripsandlegsanalysis/RailTripsAnalyzer.java
@@ -20,17 +20,23 @@
 package ch.sbb.matsim.analysis.tripsandlegsanalysis;
 
 import ch.sbb.matsim.config.variables.SBBModes.PTSubModes;
+import ch.sbb.matsim.config.variables.Variables;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Route;
+import org.matsim.core.gbl.Gbl;
+import org.matsim.core.population.routes.RouteUtils;
 import org.matsim.core.router.TripStructureUtils.Trip;
 import org.matsim.core.utils.collections.Tuple;
 import org.matsim.pt.routes.TransitPassengerRoute;
 import org.matsim.pt.transitSchedule.api.TransitLine;
+import org.matsim.pt.transitSchedule.api.TransitRoute;
 import org.matsim.pt.transitSchedule.api.TransitSchedule;
 import org.matsim.pt.transitSchedule.api.TransitStopFacility;
 
@@ -38,29 +44,45 @@ import org.matsim.pt.transitSchedule.api.TransitStopFacility;
 public class RailTripsAnalyzer {
 
     private final Set<Id<TransitLine>> railLines;
+    private final Set<Id<TransitStopFacility>> fqStops;
     private final TransitSchedule schedule;
+    private final Network network;
 
     @Inject
-    public RailTripsAnalyzer(TransitSchedule schedule) {
+    public RailTripsAnalyzer(TransitSchedule schedule, Network network) {
+        this.network = network;
         this.schedule = schedule;
         railLines = schedule.getTransitLines().values()
                 .stream()
                 .filter(l -> l.getRoutes().values().stream().anyMatch(transitRoute -> transitRoute.getTransportMode().equals(PTSubModes.RAIL)))
                 .map(transitLine -> transitLine.getId())
                 .collect(Collectors.toSet());
+        fqStops = schedule.getFacilities().values()
+                .stream()
+                .filter(transitStopFacility -> String.valueOf(transitStopFacility.getAttributes().getAttribute(Variables.FQ_RELEVANT)).equals("1"))
+                .map(transitStopFacility -> transitStopFacility.getId())
+                .collect(Collectors.toSet());
+    }
 
+    public boolean isRailLine(Id<TransitLine> transitLineId) {
+        return railLines.contains(transitLineId);
     }
 
     public double calcRailDistance(Trip trip) {
-        double rail_pm = trip.getLegsOnly().stream()
-                .filter(leg -> leg.getRoute() instanceof TransitPassengerRoute)
-                .map(leg -> (TransitPassengerRoute) leg.getRoute())
-                .filter(transitRoute -> railLines.contains(transitRoute.getLineId()))
+        double rail_pm = getRailRouteSegmentsofTrip(trip).stream()
                 .map(Route::getDistance)
                 .mapToDouble(Double::doubleValue)
                 .sum();
 
         return rail_pm;
+    }
+
+    public List<TransitPassengerRoute> getRailRouteSegmentsofTrip(Trip trip) {
+        return trip.getLegsOnly().stream()
+                .filter(leg -> leg.getRoute() instanceof TransitPassengerRoute)
+                .map(leg -> (TransitPassengerRoute) leg.getRoute())
+                .filter(transitRoute -> railLines.contains(transitRoute.getLineId()))
+                .collect(Collectors.toList());
     }
 
     public Tuple<Id<TransitStopFacility>, Id<TransitStopFacility>> getOriginDestination(Trip trip) {
@@ -85,4 +107,103 @@ public class RailTripsAnalyzer {
         return tuple;
     }
 
+    /**
+     * A rail leg between SIMBA/CH-Perimeter stops or to&from Domodossola, or the swiss section of an international rail leg
+     */
+    public double getDomesticRailDistance_m(TransitPassengerRoute transitPassengerRoute) {
+        var accessStopId = transitPassengerRoute.getAccessStopId();
+        var egressStopId = transitPassengerRoute.getEgressStopId();
+        final boolean accesIsSwissRailStop = isSwissRailStop(accessStopId);
+        final boolean egressIsSwissRailStop = isSwissRailStop(egressStopId);
+        if (accesIsSwissRailStop && egressIsSwissRailStop) {
+            return transitPassengerRoute.getDistance();
+        } else if (accesIsSwissRailStop) {
+            calcDomesticDistanceToBorder(transitPassengerRoute.getRouteId(), transitPassengerRoute.getLineId(), accessStopId);
+            return calcDomesticDistanceToBorder(transitPassengerRoute.getRouteId(), transitPassengerRoute.getLineId(), accessStopId);
+        } else if (egressIsSwissRailStop) {
+            return calcDomesticDistanceFromBorder(transitPassengerRoute.getRouteId(), transitPassengerRoute.getLineId(), egressStopId);
+        } else {
+            //entirely international or non-rail pt leg
+            return 0;
+        }
+    }
+
+    private double calcDomesticDistanceToBorder(Id<TransitRoute> routeId, Id<TransitLine> transitLineId, Id<TransitStopFacility> accessStopId) {
+        TransitRoute transitRoute = this.schedule.getTransitLines().get(transitLineId).getRoutes().get(routeId);
+        Gbl.assertNotNull(transitRoute);
+        int startIndex = transitRoute.getStops().indexOf(accessStopId);
+        final TransitStopFacility accessStop = schedule.getFacilities().get(accessStopId);
+        TransitStopFacility lastDomesticStop = accessStop;
+        for (int i = startIndex + 1; i < transitRoute.getStops().size(); i++) {
+            var currentStop = transitRoute.getStops().get(i);
+            if (isSwissRailStop(currentStop.getStopFacility().getId())) {
+                lastDomesticStop = currentStop.getStopFacility();
+            }
+
+        }
+        return RouteUtils.calcDistance(transitRoute, accessStop, lastDomesticStop, this.network);
+    }
+
+    private double calcDomesticDistanceFromBorder(Id<TransitRoute> routeId, Id<TransitLine> transitLineId, Id<TransitStopFacility> egressStopId) {
+        TransitRoute transitRoute = this.schedule.getTransitLines().get(transitLineId).getRoutes().get(routeId);
+        Gbl.assertNotNull(transitRoute);
+        int endIndex = transitRoute.getStops().indexOf(egressStopId);
+        final TransitStopFacility egressStop = schedule.getFacilities().get(egressStopId);
+        TransitStopFacility firstDomesticStop = null;
+        for (int i = 0; i < endIndex; i++) {
+            var currentStop = transitRoute.getStops().get(i);
+            if (isSwissRailStop(currentStop.getStopFacility().getId())) {
+                firstDomesticStop = currentStop.getStopFacility();
+            }
+
+        }
+        if (firstDomesticStop != null) {
+            return RouteUtils.calcDistance(transitRoute, firstDomesticStop, egressStop, this.network);
+        } else {
+            return 0.0;
+        }
+    }
+
+    public boolean isSwissRailStop(Id<TransitStopFacility> stopId) {
+        if (Variables.EXCEPTIONAL_CH_STOPS.contains(stopId)) {
+            return true;
+        }
+        var stopFacility = this.schedule.getFacilities().get(stopId);
+        Gbl.assertNotNull(stopFacility);
+        var perim = String.valueOf(stopFacility.getAttributes().getAttribute(Variables.SIMBA_CH_PERIMETER));
+        if (perim.equals("1")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isSwissRailOrFQStop(Id<TransitStopFacility> stopId) {
+        if (isSwissRailStop(stopId)) {
+            return true;
+        }
+        //some fq stops are not in CH: SBB GmbH
+        else {
+            return fqStops.contains(stopId);
+        }
+    }
+
+    //calculates according to
+    //https://confluence.sbb.ch/display/MOBIMANUAL/10.2.1+Filterung+in+MATSim
+
+    public double getFQDistance(Trip trip) {
+        List<TransitPassengerRoute> routes = getRailRouteSegmentsofTrip(trip);
+        if (routes.isEmpty()) {
+            return 0.0;
+        }
+        Id<TransitStopFacility> railAccessStop = routes.get(0).getAccessStopId();
+        Id<TransitStopFacility> railEgressStop = routes.get(routes.size() - 1).getEgressStopId();
+        if (isSwissRailOrFQStop(railAccessStop) && isSwissRailOrFQStop(railEgressStop)) {
+            boolean hasFQRelevantLeg = routes.stream().anyMatch(route -> (fqStops.contains(route.getAccessStopId()) && fqStops.contains(route.getEgressStopId())));
+            if (hasFQRelevantLeg) {
+                return routes.stream().mapToDouble(route -> route.getDistance()).sum();
+            }
+        }
+        return 0.0;
+    }
 }
