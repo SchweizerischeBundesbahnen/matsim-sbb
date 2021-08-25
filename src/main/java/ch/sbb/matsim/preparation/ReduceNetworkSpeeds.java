6945c29490e7ac79408aa1d10f1e89dfbbdb2a12
u229187 <joschka.bischoff@sbb.ch>
2021-08-25 16:40:18 +0200

    generalize script

--- src/main/java/ch/sbb/matsim/preparation/ReduceNetworkSpeeds.java
+++ src/main/java/ch/sbb/matsim/preparation/ReduceNetworkSpeeds.java
@@ -32,26 +32,33 @@ import org.matsim.core.network.io.MatsimNetworkReader;
 public class ReduceNetworkSpeeds {
 
     private final String ouputfile;
+    private final double capslow;
+    private final double capnormal;
     Zones zones;
     List<String> communities = List.of("261", "6621", "2701", "351", "5586", "230", "1061", "3203", "5226", "371");
     List<String> hvs = List.of("29", "30", "31", "32");
     List<String> nvs = List.of("50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61");
     private Network network;
 
-    public ReduceNetworkSpeeds(String networkfile, String outputfile, String shapeFile) {
+    public ReduceNetworkSpeeds(String networkfile, String outputfile, String shapeFile, double capslow, double capnormal) {
         this.ouputfile = outputfile;
         this.network = NetworkUtils.createNetwork();
         zones = ZonesLoader.loadZones("1", shapeFile, "zone_id");
         new MatsimNetworkReader(network).readFile(networkfile);
+        this.capslow = capslow;
+        this.capnormal = capnormal;
 
     }
 
     public static void main(String[] args) {
         String networkfile = args[0];
         String outputfile = args[1];
-
         String shapeFile = args[2];
-        ReduceNetworkSpeeds reduceNetworkSpeeds = new ReduceNetworkSpeeds(networkfile, outputfile, shapeFile);
+
+        double capslowcitystreets = Double.parseDouble(args[3]);
+        double capnormal = Double.parseDouble(args[4]);
+
+        ReduceNetworkSpeeds reduceNetworkSpeeds = new ReduceNetworkSpeeds(networkfile, outputfile, shapeFile, capslowcitystreets, capnormal);
         reduceNetworkSpeeds.reduce();
         reduceNetworkSpeeds.writeNetwork();
     }
@@ -69,7 +76,7 @@ public class ReduceNetworkSpeeds {
                     String munid = String.valueOf(zone.getAttribute("mun_id"));
                     if (communities.contains(munid)) {
                         //                    2030:
-                        link.setFreespeed(link.getFreespeed() * 0.9);
+                        link.setFreespeed(link.getFreespeed() * capnormal);
                         //                    2040:
                         //                    link.setFreespeed(link.getFreespeed()*0.8);
                     }
@@ -84,14 +91,14 @@ public class ReduceNetworkSpeeds {
                         if (fs > 25 / 3.6) {
 
                             //                    2030:
-                            link.setFreespeed(link.getFreespeed() * 0.8);
+                            link.setFreespeed(link.getFreespeed() * capslow);
                             //                    2040:
                             //                    link.setFreespeed(link.getFreespeed()*0.6)
                         }
                     } else {
 
                         //                    2030:
-                        link.setFreespeed(link.getFreespeed() * 0.9);
+                        link.setFreespeed(link.getFreespeed() * capnormal);
                         //                    2040:
                         //                    link.setFreespeed(link.getFreespeed()*0.8)
                     }
