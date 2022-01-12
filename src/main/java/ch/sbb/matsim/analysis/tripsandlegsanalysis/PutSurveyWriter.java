6c2af8d38d314428ecb8ae58449a8c38c80b44ed
Knupp Annette (P-V-APL-MT-MT2) <annette.knupp@sbb.ch>
2022-01-12 15:25:16 +0100

    bugfix @Inject was missing

--- src/main/java/ch/sbb/matsim/analysis/tripsandlegsanalysis/PutSurveyWriter.java
+++ src/main/java/ch/sbb/matsim/analysis/tripsandlegsanalysis/PutSurveyWriter.java
@@ -115,6 +115,7 @@ public class PutSurveyWriter {
     private final TransitSchedule schedule;
     private final Scenario scenario;
 
+    @Inject
     ExperiencedPlansService experiencedPlansService;
 
     @Inject
