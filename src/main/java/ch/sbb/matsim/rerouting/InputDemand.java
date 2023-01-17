75cbebe7a6afce5a94fce68cb21d5149b3abe7ca
u239380 <marc.dziakowski@sbb.ch>
2023-01-17 16:42:47 +0100

    parallel stream calculation ignores missing stations

--- src/main/java/ch/sbb/matsim/rerouting/InputDemand.java
+++ src/main/java/ch/sbb/matsim/rerouting/InputDemand.java
@@ -4,100 +4,80 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import omx.OmxFile;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.pt.transitSchedule.api.TransitStopFacility;
 
 public class InputDemand {
-
-    private final List<Integer> codeList;
     private final OmxFile omxFile;
-    private final int lastPosition;
-    private final int lastDemandSegment;
-    private int time = 1;
+    private final Map<Integer, Coord> validPosistions;
     private final List<Integer> timeList = new ArrayList<>();
-    private int xPosition = 0;
-    private int yPosition = 0;
 
-    InputDemand(String columNames, String nachfrageTag) {
-        this.codeList = readColumNames(columNames);
+    InputDemand(String columNames, String nachfrageTag, Scenario scenario) {
         this.omxFile = readOMXMatrciesDayDemand(nachfrageTag);
-        this.lastPosition = this.codeList.size();
-        this.lastDemandSegment = this.omxFile.getMatrixNames().size();
-        for (int i = 1; i < lastDemandSegment; i++) {
+        Map<Integer, Coord> assignmentMap = readAssignment(columNames, scenario);
+        this.validPosistions = searchValidStationsPosition((int[]) omxFile.getLookup("NO").getLookup(), assignmentMap);
+        for (int i = 1; i < this.omxFile.getMatrixNames().size(); i++) {
             timeList.add(i);
         }
     }
 
-    private static List<Integer> readColumNames(String file) {
-        List<Integer> codeList = new ArrayList<>();
+    private Map<Integer, Coord> searchValidStationsPosition(int[] lookup, Map<Integer, Coord> assignmentMap) {
+        Map<Integer, Coord> validPostions = new HashMap<>();
+        for (int i = 0; i < lookup.length; i++) {
+            if (assignmentMap.containsKey(lookup[i])) {
+                validPostions.put(i, assignmentMap.get(lookup[i]));
+            }
+        }
+        return validPostions;
+    }
+
+    private Map<Integer, Coord> readAssignment(String file, Scenario scenario) {
+        Map<Integer, Coord> assignmentMap = new HashMap<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             List<String> header = List.of(reader.readLine().split(";"));
             String line;
             while ((line = reader.readLine()) != null) {
-                codeList.add(Integer.parseInt(line.split(";")[header.indexOf("id")]));
+                String[] splitLine = line.split(";");
+                if (scenario.getTransitSchedule().getFacilities().get(Id.create(splitLine[1], TransitStopFacility.class)) == null) {
+                    continue;
+                }
+                Coord coord = assignmentMap.get(Integer.parseInt(splitLine[0]));
+                if (coord == null) {
+                    assignmentMap.put(Integer.parseInt(splitLine[0]), scenario.getTransitSchedule().getFacilities().get(Id.create(splitLine[1], TransitStopFacility.class)).getCoord());
+                } else {
+                    assignmentMap.put(Integer.parseInt(splitLine[0]),
+                        new Coord((scenario.getTransitSchedule().getFacilities().get(Id.create(splitLine[1], TransitStopFacility.class)).getCoord().getX() + coord.getX()) / 2,
+                            (scenario.getTransitSchedule().getFacilities().get(Id.create(splitLine[1], TransitStopFacility.class)).getCoord().getY() + coord.getY()) / 2));
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return codeList;
+        return assignmentMap;
     }
 
-    private static OmxFile readOMXMatrciesDayDemand(String file) {
+    private OmxFile readOMXMatrciesDayDemand(String file) {
         OmxFile omxFile = new OmxFile(file);
         omxFile.openReadOnly();
         omxFile.summary();
         return omxFile;
     }
 
-    public int getLastPosition() {
-        return lastPosition;
-    }
-
-    public int getLastDemandSegment() {
-        return lastDemandSegment;
-    }
-
-    public List<Integer> getCodeList() {
-        return codeList;
-    }
-
     public OmxFile getOmxFile() {
         return omxFile;
     }
 
-    public int getTime() {
-        return time;
-    }
-
-    public int getXPosition() {
-        return xPosition;
-    }
-
-    public int getYPosition() {
-        return yPosition;
-    }
-
-    public void increaseTime() {
-        this.time++;
-    }
-
-    public void increaseXPosition() {
-        this.xPosition++;
-    }
-
-    public void increaseYPosition() {
-        this.yPosition++;
-    }
-
-    public void resetXPosition() {
-        this.xPosition = 0;
-    }
-
-    public void resetYPosition() {
-        this.yPosition = 0;
-    }
-
     public List<Integer> getTimeList() {
         return timeList;
+    }
+
+    public Map<Integer, Coord> getValidPosistions() {
+        return validPosistions;
     }
 }
--- src/main/java/ch/sbb/matsim/rerouting/MatrixRouter.java
+++ src/main/java/ch/sbb/matsim/rerouting/MatrixRouter.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map.Entry;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
@@ -48,24 +48,25 @@ import org.matsim.facilities.ActivityFacility;
 import org.matsim.facilities.Facility;
 import org.matsim.pt.routes.TransitPassengerRoute;
 import org.matsim.pt.transitSchedule.api.TransitScheduleReader;
-import org.matsim.pt.transitSchedule.api.TransitStopFacility;
 
 public class MatrixRouter {
 
-    final static String columNames = "Z:/99_Playgrounds/MD/Umlegung/Input/ColumNames.csv";
+    final static String columNames = "Z:/99_Playgrounds/MD/Umlegung/Input/ZoneToNode.csv";
     final static String demand = "Z:/99_Playgrounds/MD/Umlegung/Input/NachfrageTag.omx";
     final static String saveFileInpout = "Z:/99_Playgrounds/MD/Umlegung/Input/saveFile.csv";
     final static String schedualFile = "Z:/99_Playgrounds/MD/Umlegung/Input/smallTransitSchedule.xml.gz";
     final static String netwoekFile = "Z:/99_Playgrounds/MD/Umlegung/Input//smallTransitNetwork.xml.gz";
-    final static String output = "Z:/99_Playgrounds/MD/Umlegung/Results/saveFileWithDemand.csv";
+    final static String output = "Z:/99_Playgrounds/MD/Umlegung/Results/test.csv";
 
-    final InputDemand inputDemand = new InputDemand(columNames, demand);
-    final ConcurrentHashMap<Id<Link>, DemandStorage> idDemandStorageMap = createLinkDemandStorage();
+    final InputDemand inputDemand;
+    final Map<Id<Link>, DemandStorage> idDemandStorageMap = createLinkDemandStorage();
     final ActivityFacilitiesFactory afFactory = ScenarioUtils.createScenario(ConfigUtils.createConfig()).getActivityFacilities().getFactory();
-     final Scenario scenario;
+    final Scenario scenario;
     final SwissRailRaptor swissRailRaptor;
     final RailTripsAnalyzer railTripsAnalyzer;
-    SwissRailRaptorData data;
+    final SwissRailRaptorData data;
+
+    int count = 0;
 
     SBBIntermodalRaptorStopFinder stopFinder;
     RaptorParametersForPerson raptorParametersForPerson;
@@ -82,7 +83,6 @@ public class MatrixRouter {
     }
 
     public MatrixRouter() {
-        System.out.println("Config");
         Config config = ConfigUtils.createConfig();
 
         SwissRailRaptorConfigGroup srrConfig = ConfigUtils.addOrGetModule(config, SwissRailRaptorConfigGroup.class);
@@ -117,10 +117,12 @@ public class MatrixRouter {
 
         this.swissRailRaptor = new SwissRailRaptor(data, raptorParametersForPerson, routeSelector, stopFinder, inVehicleCostCalculator, transferCostCalculator);
         this.railTripsAnalyzer = new RailTripsAnalyzer(scenario.getTransitSchedule(), scenario.getNetwork());
+
+        this.inputDemand = new InputDemand(columNames, demand, scenario);
     }
 
     public void routingWithBestPath() {
-       inputDemand.getTimeList().stream().forEach(this::calculateMatrix);
+       inputDemand.getTimeList().stream().parallel().forEach(this::calculateMatrix);
        writeLinkCount();
     }
 
@@ -128,25 +130,18 @@ public class MatrixRouter {
         System.out.println("Matrix: " + time);
         var raptor = new SwissRailRaptor(data, raptorParametersForPerson, routeSelector, stopFinder, inVehicleCostCalculator, transferCostCalculator);
         double[][] matrix = (double[][]) inputDemand.getOmxFile().getMatrix(time.toString()).getData();
-        for (int x = 0; x < inputDemand.getLastPosition(); x++) {
-            if (scenario.getTransitSchedule().getFacilities().get(Id.create(inputDemand.getCodeList().get(x), TransitStopFacility.class)) == null) {
-                continue;
-            }
-
-            for (int y = 0; y < inputDemand.getLastPosition(); y++) {
-                if (scenario.getTransitSchedule().getFacilities().get(Id.create(inputDemand.getCodeList().get(y), TransitStopFacility.class)) == null) {
-                    continue;
-                }
-                double timeDemand = matrix[x][y];
+        for (Entry<Integer, Coord> entryX : inputDemand.getValidPosistions().entrySet()) {
+            for (Entry<Integer, Coord> entryY : inputDemand.getValidPosistions().entrySet()) {
+                double timeDemand = matrix[entryX.getKey()][entryY.getKey()];
                 if (timeDemand != 0) {
-                    Coord startCoord = scenario.getTransitSchedule().getFacilities().get(Id.create(inputDemand.getCodeList().get(x), TransitStopFacility.class)).getCoord();
-                    Coord endCoord = scenario.getTransitSchedule().getFacilities().get(Id.create(inputDemand.getCodeList().get(y), TransitStopFacility.class)).getCoord();
-                    Facility startF = afFactory.createActivityFacility(Id.create(1, ActivityFacility.class), startCoord);
-                    Facility endF = afFactory.createActivityFacility(Id.create(2, ActivityFacility.class), endCoord);
-                    RoutingRequest request = DefaultRoutingRequest.withoutAttributes(startF, endF, inputDemand.getTime() * 600, null);
+                    Facility startF = afFactory.createActivityFacility(Id.create(1, ActivityFacility.class), entryX.getValue());
+                    Facility endF = afFactory.createActivityFacility(Id.create(2, ActivityFacility.class), entryY.getValue());
+                    RoutingRequest request = DefaultRoutingRequest.withoutAttributes(startF, endF, (time - 1) * 600, null);
                     List<? extends PlanElement> legs = raptor.calcRoute(request);
                     if (legs == null) {
-                        //System.out.println("No connection found for " + inputDemand.getCodeList().get(inputDemand.getXPosition()) + " to " + inputDemand.getCodeList().get(inputDemand.getYPosition()));
+                        //System.out.println("No connection found for " + entryX.getValue() + " to " + entryX.getValue() + " at time " + time + " demand " + timeDemand);
+                        //System.out.println("LINESTRING (" + startCoord.getX() + " " + startCoord.getY() + ", " + endCoord.getX() + " " + endCoord.getY() + ");" + time);
+                        count++;
                         continue;
                     }
                     for (PlanElement pe : legs) {
@@ -180,8 +175,8 @@ public class MatrixRouter {
         }
     }
 
-    private static ConcurrentHashMap<Id<Link>, DemandStorage> createLinkDemandStorage() {
-        ConcurrentHashMap<Id<Link>, DemandStorage> idDemandStorageMap = new ConcurrentHashMap<>();
+    private static Map<Id<Link>, DemandStorage> createLinkDemandStorage() {
+        Map<Id<Link>, DemandStorage> idDemandStorageMap = new HashMap<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(saveFileInpout))) {
             List<String> header = List.of(reader.readLine().split(";"));
             String line;
