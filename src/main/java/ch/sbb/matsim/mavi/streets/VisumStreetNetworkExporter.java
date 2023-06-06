3ddda687165a60d5db2946f19397b4950c330fe4
Knupp Annette (P-V-APL-MT-MT2) <annette.knupp@sbb.ch>
2023-06-06 11:27:23 +0200

    reformat code

--- src/main/java/ch/sbb/matsim/mavi/streets/VisumStreetNetworkExporter.java
+++ src/main/java/ch/sbb/matsim/mavi/streets/VisumStreetNetworkExporter.java
@@ -30,171 +30,171 @@ import java.util.Set;
 
 public class VisumStreetNetworkExporter {
 
-	private final static Logger log = LogManager.getLogger(VisumStreetNetworkExporter.class);
-	private final Map<Id<Link>, String> wktLineStringPerVisumLink = new HashMap<>();
-	public static final Set fullModeset = Set.of(SBBModes.CAR, SBBModes.RIDE, SBBModes.BIKE);
-	public static final Set<String> modeSetWithoutBike = Set.of(SBBModes.CAR, SBBModes.RIDE);
-	public static final Set<String> modeSetBikeOnly = Set.of(SBBModes.BIKE);
-	private Scenario scenario;
-	private NetworkFactory nf;
+    private final static Logger log = LogManager.getLogger(VisumStreetNetworkExporter.class);
+    private final Map<Id<Link>, String> wktLineStringPerVisumLink = new HashMap<>();
+    public static final Set fullModeset = Set.of(SBBModes.CAR, SBBModes.RIDE, SBBModes.BIKE);
+    public static final Set<String> modeSetWithoutBike = Set.of(SBBModes.CAR, SBBModes.RIDE);
+    public static final Set<String> modeSetBikeOnly = Set.of(SBBModes.BIKE);
+    private Scenario scenario;
+    private NetworkFactory nf;
 
-	public static Id<Link> createLinkId(String fromNode, String visumLinkId) {
-		return Id.createLinkId(Integer.toString(Integer.parseInt(fromNode), 36) + "_" + Integer.toString(Integer.parseInt(visumLinkId), 36));
-	}
+    public static Id<Link> createLinkId(String fromNode, String visumLinkId) {
+        return Id.createLinkId(Integer.toString(Integer.parseInt(fromNode), 36) + "_" + Integer.toString(Integer.parseInt(visumLinkId), 36));
+    }
 
-	public static Tuple<Integer, Integer> extractVisumNodeAndLinkId(Id<Link> linkId) {
-		try {
-			int visumFromNodeId = Integer.parseInt(linkId.toString().split("_")[0], 36);
-			int visumLinkId = Integer.parseInt(linkId.toString().split("_")[1], 36);
-			return Tuple.of(visumFromNodeId, visumLinkId);
-		} catch (NumberFormatException e) {
-			return null;
-		}
-	}
+    public static Tuple<Integer, Integer> extractVisumNodeAndLinkId(Id<Link> linkId) {
+        try {
+            int visumFromNodeId = Integer.parseInt(linkId.toString().split("_")[0], 36);
+            int visumLinkId = Integer.parseInt(linkId.toString().split("_")[1], 36);
+            return Tuple.of(visumFromNodeId, visumLinkId);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
 
-	public void run(String inputvisum, String outputPath, int visumVersion, boolean exportCounts, boolean exportPolylines) throws IOException {
-		ActiveXComponent visum = new ActiveXComponent("Visum.Visum." + visumVersion);
-		log.info("VISUM Client gestartet.");
-		Dispatch.call(visum, "LoadVersion", inputvisum);
+    public void run(String inputvisum, String outputPath, int visumVersion, boolean exportCounts, boolean exportPolylines) throws IOException {
+        ActiveXComponent visum = new ActiveXComponent("Visum.Visum." + visumVersion);
+        log.info("VISUM Client gestartet.");
+        Dispatch.call(visum, "LoadVersion", inputvisum);
 
-		this.scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
-		this.nf = scenario.getNetwork().getFactory();
+        this.scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        this.nf = scenario.getNetwork().getFactory();
 
-		Dispatch net = Dispatch.get(visum, "Net").toDispatch();
+        Dispatch net = Dispatch.get(visum, "Net").toDispatch();
 
-		Dispatch filters = Dispatch.get(visum, "Filters").toDispatch();
-		Dispatch.call(filters, "InitAll");
-		if (exportCounts) {
-			this.exportCountStations(visum, outputPath);
-		}
-		String[][] nodes = importNodes(net, "No", "XCoord", "YCoord");
-		String[][] links = importLinks(net, "FromNodeNo", "ToNodeNo", "Length", "CapPrT", "V0PrT", "TypeNo",
-				"NumLanes", "TSysSet", Variables.ACCESS_CONTROLLED, "WKTPoly", "No");
-		createNetwork(nodes, links);
-		// Export Polylines
-		if (exportPolylines) {
-			new PolylinesCreator().runStreets(this.scenario.getNetwork(), wktLineStringPerVisumLink, "polylines.csv", outputPath);
-		}
+        Dispatch filters = Dispatch.get(visum, "Filters").toDispatch();
+        Dispatch.call(filters, "InitAll");
+        if (exportCounts) {
+            this.exportCountStations(visum, outputPath);
+        }
+        String[][] nodes = importNodes(net, "No", "XCoord", "YCoord");
+        String[][] links = importLinks(net, "FromNodeNo", "ToNodeNo", "Length", "CapPrT", "V0PrT", "TypeNo",
+                "NumLanes", "TSysSet", Variables.ACCESS_CONTROLLED, "WKTPoly", "No");
+        createNetwork(nodes, links);
+        // Export Polylines
+        if (exportPolylines) {
+            new PolylinesCreator().runStreets(this.scenario.getNetwork(), wktLineStringPerVisumLink, "polylines.csv", outputPath);
+        }
 
-	}
+    }
 
 
-	private void exportCountStations(Dispatch net, String outputFolder) throws IOException {
-		VisumToCounts visumToCounts = new VisumToCounts();
+    private void exportCountStations(Dispatch net, String outputFolder) throws IOException {
+        VisumToCounts visumToCounts = new VisumToCounts();
 
-		File file = new File(outputFolder, "counts");
-		File csv = new File(outputFolder, "counts.csv");
-		visumToCounts.exportCountStations(net, file.getAbsolutePath(), csv.getAbsolutePath());
-	}
+        File file = new File(outputFolder, "counts");
+        File csv = new File(outputFolder, "counts.csv");
+        visumToCounts.exportCountStations(net, file.getAbsolutePath(), csv.getAbsolutePath());
+    }
 
-	private String[][] importNodes(Dispatch net, String... attribute) {
-		Dispatch nodes = Dispatch.get(net, "Nodes").toDispatch();//import nodes
-		return this.toArray(nodes, attribute);
-	}
+    private String[][] importNodes(Dispatch net, String... attribute) {
+        Dispatch nodes = Dispatch.get(net, "Nodes").toDispatch();//import nodes
+        return this.toArray(nodes, attribute);
+    }
 
-	private String[][] importLinks(Dispatch net, String... attribute) {
-		Dispatch links = Dispatch.get(net, "Links").toDispatch();
-		return toArray(links, attribute);
-	}
+    private String[][] importLinks(Dispatch net, String... attribute) {
+        Dispatch links = Dispatch.get(net, "Links").toDispatch();
+        return toArray(links, attribute);
+    }
 
-	private String[][] toArray(Dispatch objects, String... attributes) {
-		int n = Integer.parseInt(Dispatch.call(objects, "CountActive").toString()); //number of nodes
+    private String[][] toArray(Dispatch objects, String... attributes) {
+        int n = Integer.parseInt(Dispatch.call(objects, "CountActive").toString()); //number of nodes
 
-		String[][] attarray = new String[n][attributes.length]; //2d array containing all attributes of all nodes
-		int j = 0;
+        String[][] attarray = new String[n][attributes.length]; //2d array containing all attributes of all nodes
+        int j = 0;
 
-		for (String att : attributes) {
-			log.info(att);
-			SafeArray a = Dispatch.call(objects, "GetMultiAttValues", att).toSafeArray();
-			int i = 0;
-			while (i < n) {
-				attarray[i][j] = a.getString(i, 1);
-				i++;
-			}
-			log.info("done");
-			j++;
-		}
-		return attarray;
-	}
+        for (String att : attributes) {
+            log.info(att);
+            SafeArray a = Dispatch.call(objects, "GetMultiAttValues", att).toSafeArray();
+            int i = 0;
+            while (i < n) {
+                attarray[i][j] = a.getString(i, 1);
+                i++;
+            }
+            log.info("done");
+            j++;
+        }
+        return attarray;
+    }
 
-	private void createNetwork(String[][] attarraynode, String[][] attarraylink) {
-		Network network = this.scenario.getNetwork();
-		network.setCapacityPeriod(3600);
+    private void createNetwork(String[][] attarraynode, String[][] attarraylink) {
+        Network network = this.scenario.getNetwork();
+        network.setCapacityPeriod(3600);
 
-		for (String[] anAttarraynode : attarraynode) {
-			Coord coord = new Coord(Double.parseDouble(anAttarraynode[1]),
-					Double.parseDouble(anAttarraynode[2]));
-			Node node = nf.createNode(Id.createNodeId("C_" + anAttarraynode[0]), coord);
-			network.addNode(node);
-		}
+        for (String[] anAttarraynode : attarraynode) {
+            Coord coord = new Coord(Double.parseDouble(anAttarraynode[1]),
+                    Double.parseDouble(anAttarraynode[2]));
+            Node node = nf.createNode(Id.createNodeId("C_" + anAttarraynode[0]), coord);
+            network.addNode(node);
+        }
 
-		boolean carAllowed;
-		boolean bikeAllowed;
-		for (String[] anAttarraylink : attarraylink) {
-			carAllowed = (anAttarraylink[7].contains("P"));
-			bikeAllowed = (anAttarraylink[7].contains("V"));
-			if (carAllowed | bikeAllowed) {
-				final String fromNode = anAttarraylink[0];
-				final String toNode = anAttarraylink[1];
-				final String visumLinkNo = anAttarraylink[10];
-				Id<Link> id = createLinkId(fromNode, visumLinkNo);
-				Link link = createLink(id, fromNode, toNode, Double.parseDouble(anAttarraylink[2]),
-						Double.parseDouble(anAttarraylink[3]), (Double.parseDouble(anAttarraylink[4])),
-						Integer.parseInt(anAttarraylink[6]));
-				if (link != null) {
-					NetworkUtils.setType(link, anAttarraylink[5]);
-					int ac;
-					try {
-						ac = Integer.parseInt(anAttarraylink[8]);
-					} catch (NumberFormatException e) {
-						log.warn("Access Control not defined for link " + link.getId() + ". Assuming = 1");
-						ac = 1;
-					}
-					link.getAttributes().putAttribute(Variables.ACCESS_CONTROLLED, ac);
-					if (ac == 1) {
-						link.setAllowedModes(modeSetWithoutBike);
-					} else if (!carAllowed) {
-						link.setAllowedModes(modeSetBikeOnly);
-					}
-					network.addLink(link);
-				}
-				this.wktLineStringPerVisumLink.put(id, anAttarraylink[9]);
-			}
-		}
-	}
+        boolean carAllowed;
+        boolean bikeAllowed;
+        for (String[] anAttarraylink : attarraylink) {
+            carAllowed = (anAttarraylink[7].contains("P"));
+            bikeAllowed = (anAttarraylink[7].contains("V"));
+            if (carAllowed | bikeAllowed) {
+                final String fromNode = anAttarraylink[0];
+                final String toNode = anAttarraylink[1];
+                final String visumLinkNo = anAttarraylink[10];
+                Id<Link> id = createLinkId(fromNode, visumLinkNo);
+                Link link = createLink(id, fromNode, toNode, Double.parseDouble(anAttarraylink[2]),
+                        Double.parseDouble(anAttarraylink[3]), (Double.parseDouble(anAttarraylink[4])),
+                        Integer.parseInt(anAttarraylink[6]));
+                if (link != null) {
+                    NetworkUtils.setType(link, anAttarraylink[5]);
+                    int ac;
+                    try {
+                        ac = Integer.parseInt(anAttarraylink[8]);
+                    } catch (NumberFormatException e) {
+                        log.warn("Access Control not defined for link " + link.getId() + ". Assuming = 1");
+                        ac = 1;
+                    }
+                    link.getAttributes().putAttribute(Variables.ACCESS_CONTROLLED, ac);
+                    if (ac == 1) {
+                        link.setAllowedModes(modeSetWithoutBike);
+                    } else if (!carAllowed) {
+                        link.setAllowedModes(modeSetBikeOnly);
+                    }
+                    network.addLink(link);
+                }
+                this.wktLineStringPerVisumLink.put(id, anAttarraylink[9]);
+            }
+        }
+    }
 
-	private Link createLink(Id<Link> id, String fromNode, String toNode, double length, double cap, double v, int numlanes) {
-		Node fnode = scenario.getNetwork().getNodes().get(Id.createNodeId("C_" + fromNode));
-		Node tnode = scenario.getNetwork().getNodes().get(Id.createNodeId("C_" + toNode));
+    private Link createLink(Id<Link> id, String fromNode, String toNode, double length, double cap, double v, int numlanes) {
+        Node fnode = scenario.getNetwork().getNodes().get(Id.createNodeId("C_" + fromNode));
+        Node tnode = scenario.getNetwork().getNodes().get(Id.createNodeId("C_" + toNode));
 
-		if (fnode == null || tnode == null) {
-			return null;
-		}
+        if (fnode == null || tnode == null) {
+            return null;
+        }
 
-		Link link = nf.createLink(id, fnode, tnode);
-		if (length < 0.01) {
-			length = 0.01;
-		}
-		if (numlanes < 1) {
-			numlanes = 1;
-		}
-		length *= 1000.;
-		double beelineDistance = CoordUtils.calcEuclideanDistance(fnode.getCoord(), tnode.getCoord());
-		if (length < beelineDistance) {
-			if (beelineDistance - length > 1.0) {
-				log.warn(link.getId() + " has a length (" + length + ") shorter than its beeline distance (" + beelineDistance + "). Will not correct this.");
-			}
-		}
-		link.setLength(length);
-		link.setCapacity(cap);
-		link.setFreespeed(v / 3.6);
-		link.setNumberOfLanes(numlanes);
-		link.setAllowedModes(fullModeset);
+        Link link = nf.createLink(id, fnode, tnode);
+        if (length < 0.01) {
+            length = 0.01;
+        }
+        if (numlanes < 1) {
+            numlanes = 1;
+        }
+        length *= 1000.;
+        double beelineDistance = CoordUtils.calcEuclideanDistance(fnode.getCoord(), tnode.getCoord());
+        if (length < beelineDistance) {
+            if (beelineDistance - length > 1.0) {
+                log.warn(link.getId() + " has a length (" + length + ") shorter than its beeline distance (" + beelineDistance + "). Will not correct this.");
+            }
+        }
+        link.setLength(length);
+        link.setCapacity(cap);
+        link.setFreespeed(v / 3.6);
+        link.setNumberOfLanes(numlanes);
+        link.setAllowedModes(fullModeset);
 
-		return link;
-	}
+        return link;
+    }
 
-	public Network getNetwork() {
-		return scenario.getNetwork();
-	}
+    public Network getNetwork() {
+        return scenario.getNetwork();
+    }
 }
