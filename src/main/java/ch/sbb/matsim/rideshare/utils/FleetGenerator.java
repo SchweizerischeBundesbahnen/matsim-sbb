ee941e7af3d311980fcb3ec41c34537d668cd88c
Joschka Bischoff <joschka.bischoff@sbb.ch>
2024-01-05 13:06:44 +0100

    minor

--- src/main/java/ch/sbb/matsim/rideshare/utils/FleetGenerator.java
+++ src/main/java/ch/sbb/matsim/rideshare/utils/FleetGenerator.java
@@ -1,5 +1,6 @@
 package ch.sbb.matsim.rideshare.utils;
 
+import ch.sbb.matsim.config.variables.SBBModes;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
@@ -15,6 +16,7 @@ import org.matsim.core.scenario.ScenarioUtils;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class FleetGenerator {
 
@@ -22,14 +24,14 @@ public class FleetGenerator {
 	 * Adjust these variables and paths to your need.
 	 */
 
-	private static final int numberOfVehicles = 100;
-	private static final int seatsPerVehicle = 2; //this is important for DRT, value is not used by taxi
+	private static final int numberOfVehicles = 1000;
+	private static final int seatsPerVehicle = 8;
 	private static final double operationStartTime = 0.0;
 	private static final double operationEndTime = 30d * 60d * 60d; //24h
 	private static final Random random = MatsimRandom.getRandom();
 
-	private static final Path networkFile = Paths.get("\\\\k13536\\mobi\\40_Projekte\\20190913_Ridesharing\\sim\\neuchatel\\input\\network.xml.gz");
-	private static final Path outputFile = Paths.get("\\\\k13536\\mobi\\40_Projekte\\20190913_Ridesharing\\sim\\neuchatel\\input\\fleetVehicles_feeder_ne.xml");
+	private static final Path networkFile = Paths.get("\\\\wsbbrz0283\\mobi\\40_Projekte\\20221221_Postauto_OnDemand\\20221221_Appenzell\\streets\\output\\network.xml.gz");
+	private static final Path outputFile = Paths.get("\\\\wsbbrz0283\\mobi\\40_Projekte\\20221221_Postauto_OnDemand\\20221221_Appenzell\\sim\\1000veh\\fleetVehicles.xml");
 
 	public static void main(String[] args) {
 
@@ -40,18 +42,19 @@ public class FleetGenerator {
 
 		Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
 		new MatsimNetworkReader(scenario.getNetwork()).readFile(networkFile.toString());
-		//        List<Id<Link>> availableLinks  = scenario.getNetwork().getLinks().entrySet().stream()
-		//              .filter(entry -> entry.getValue().getAllowedModes().contains(SBBModes.CAR)).map(e->e.getKey()).collect(Collectors.toList());
-		//        List<Id<Link>> availableLinks = Arrays.asList(Id.createLinkId(177382)); //lcf
-		List<Id<Link>> availableLinks = List.of(Id.createLinkId(598836)); //ne
+		List<Id<Link>> availableLinks = scenario.getNetwork().getLinks().entrySet().stream()
+				.filter(entry -> entry.getValue().getAllowedModes().contains(SBBModes.CAR)).map(e -> e.getKey()).collect(Collectors.toList());
+
 		Set<DvrpVehicleSpecification> vehicleSpecifications = new HashSet<>();
 		for (int z = 0; z < numberOfVehicles; z++) {
+			double startTime = operationStartTime + random.nextInt(12) * 3600;
+			double endTime = startTime + 16 * 3600;
 			vehicleSpecifications.add(ImmutableDvrpVehicleSpecification.newBuilder()
 					.id(Id.create("drt_" + z, DvrpVehicle.class))
 					.startLinkId(availableLinks.get(random.nextInt(availableLinks.size())))
 					.capacity(seatsPerVehicle)
-					.serviceBeginTime(operationStartTime)
-					.serviceEndTime(operationEndTime)
+					.serviceBeginTime(startTime)
+					.serviceEndTime(endTime)
 					.build());
 		}
 		new FleetWriter(vehicleSpecifications.stream().sorted(Comparator.comparing(v -> v.getId().toString()))).write(outputFile.toString());
