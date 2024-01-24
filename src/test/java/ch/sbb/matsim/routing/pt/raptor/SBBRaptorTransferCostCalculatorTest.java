9ea2cc98d3c84b0816cf70131fdaccac6626cd88
Knupp Annette (P-V-APL-MT-MT2) <annette.knupp@sbb.ch>
2024-01-24 17:32:26 +0100

    erster wirklicher Ansatz zu einem Test

--- src/test/java/ch/sbb/matsim/routing/pt/raptor/SBBRaptorTransferCostCalculatorTest.java
+++ src/test/java/ch/sbb/matsim/routing/pt/raptor/SBBRaptorTransferCostCalculatorTest.java
@@ -1,99 +1,86 @@
 package ch.sbb.matsim.routing.pt.raptor;
 
-import ch.sbb.matsim.config.SBBAccessTimeConfigGroup;
-import ch.sbb.matsim.config.variables.SBBModes;
+import ch.sbb.matsim.analysis.SBBDefaultAnalysisListener;
+import ch.sbb.matsim.config.SwissRailRaptorConfigGroup;
+import ch.sbb.matsim.replanning.SBBPermissibleModesCalculator;
+import ch.sbb.matsim.replanning.SBBSubtourModeChoice;
 import ch.sbb.matsim.routing.SBBAnalysisMainModeIdentifier;
-import ch.sbb.matsim.routing.access.AccessEgressModule;
-import ch.sbb.matsim.routing.network.SBBNetworkRoutingModule;
-import ch.sbb.matsim.zones.ZonesModule;
 import junit.framework.TestCase;
+import org.junit.Test;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.*;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.ScoringConfigGroup;
-import org.matsim.core.router.MainModeIdentifier;
-import org.matsim.core.scenario.ScenarioUtils;
-import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorCore.PathElement;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.population.algorithms.PermissibleModesCalculator;
+import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
+import org.matsim.core.router.AnalysisMainModeIdentifier;
+import org.matsim.core.router.DefaultRoutingRequest;
+import org.matsim.core.utils.geometry.CoordUtils;
+import org.matsim.pt.router.TransitRouter;
+import org.matsim.pt.routes.TransitPassengerRoute;
+import org.matsim.pt.transitSchedule.api.*;
+import org.matsim.testcases.MatsimTestUtils;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class SBBRaptorTransferCostCalculatorTest extends TestCase {
 
 	SwissRailRaptorCore c;
+
 	public void testCalcTransferCost() {
 	}
 
 	public void testGetFirstPEOfTripPart() {
 	}
-	
-	public void testFixture(String mode, int start) {
 
-		Config config = ConfigUtils.createConfig(new SBBAccessTimeConfigGroup());
-		config.controller().setOutputDirectory("test/output/AccessTimeIntegrationTest");
-		Scenario scenario = ScenarioUtils.createScenario(config);
-		Population population = scenario.getPopulation();
-		Network network = scenario.getNetwork();
-
-		Set<String> linkModes = new HashSet<>();
-		linkModes.add(mode);
-
-		for (Link link : network.getLinks().values()) {
-			link.setAllowedModes(linkModes);
-		}
-
-		PopulationFactory pf = population.getFactory();
-		Plan plan = pf.createPlan();
-
-		Person person = pf.createPerson(Id.createPersonId("1"));
-
-		Leg leg = pf.createLeg(mode);
-
-		plan.addLeg(leg);
-
-		Leg leg2 = pf.createLeg(mode);
-		plan.addLeg(leg2);
-
-		person.addPlan(plan);
-		person.setSelectedPlan(plan);
-
-		population.addPerson(person);
-
-		ScoringConfigGroup.ActivityParams params = new ScoringConfigGroup.ActivityParams("home");
-		params.setScoringThisActivityAtAll(false);
-		scenario.getConfig().scoring().addActivityParams(params);
-
-		ScoringConfigGroup.ActivityParams params2 = new ScoringConfigGroup.ActivityParams("work");
-		params2.setScoringThisActivityAtAll(false);
-		scenario.getConfig().scoring().addActivityParams(params2);
-
-		ScoringConfigGroup.ActivityParams params3 = new ScoringConfigGroup.ActivityParams(mode + " interaction");
-		params3.setScoringThisActivityAtAll(false);
-		scenario.getConfig().scoring().addActivityParams(params3);
-		var rideParams = scenario.getConfig().routing().getTeleportedModeParams().get(SBBModes.RIDE);
-		scenario.getConfig().routing().removeParameterSet(rideParams);
-
-		//config.routing().setNetworkModes(List.of(SBBModes.CAR,SBBModes.RIDE));
-		SBBNetworkRoutingModule.prepareScenario(scenario);
-		ZonesModule.addZonestoScenario(scenario);
-		AccessEgressModule.prepareLinkAttributes(scenario, false);
-
-		Leg l1 = pf.createLeg(SBBModes.PT);
-		Leg l2 = pf.createLeg(SBBModes.ACCESS_EGRESS_WALK);
-		Leg l3 = pf.createLeg(SBBModes.CAR);
-		Leg l4 = pf.createLeg(SBBModes.RIDEFEEDER);
-		Leg l5 = pf.createLeg(SBBModes.WALK_MAIN_MAINMODE);
-		Activity a = pf.createActivityFromCoord("test", new Coord(0, 0));
-		MainModeIdentifier mainModeIdentifier = new SBBAnalysisMainModeIdentifier();
-
-		List<PlanElement> ptTrip = List.of(l1, a, l2);
-
-
+	private SwissRailRaptor createTransitRouter(TransitSchedule schedule, Config config, Network network) {
+		SwissRailRaptorData data = SwissRailRaptorData.create(schedule, null, RaptorUtils.createStaticConfig(config), network, null);
+		SwissRailRaptor raptor = new SwissRailRaptor.Builder(data, config).with(new SBBRaptorTransferCostCalculator()).build();
+		return raptor;
 	}
+
+	@Test
+	public void testLineChange() {
+		Fixture f = new Fixture();
+		f.init();
+		ConfigUtils.addOrGetModule(f.config, SwissRailRaptorConfigGroup.class).addModeToModeTransferPenalty(new SwissRailRaptorConfigGroup.ModeToModeTransferPenalty("train", "bus", 50.0));
+		ConfigUtils.addOrGetModule(f.config, SwissRailRaptorConfigGroup.class).addModeToModeTransferPenalty(new SwissRailRaptorConfigGroup.ModeToModeTransferPenalty("bus", "train", 50.0));
+		ConfigUtils.addOrGetModule(f.config, SwissRailRaptorConfigGroup.class).addModeToModeTransferPenalty(new SwissRailRaptorConfigGroup.ModeToModeTransferPenalty("bus", "bus", 100.0));
+		RaptorParameters raptorParams = RaptorUtils.createParameters(f.config);
+		TransitRouter router = createTransitRouter(f.schedule, f.config, f.network);
+		Coord toCoord = new Coord(28100, 5050);
+		List<? extends PlanElement> legs = router.calcRoute(DefaultRoutingRequest.withoutAttributes(new FakeFacility(new Coord(24100, 50)), new FakeFacility(toCoord), 6.0 * 3600, null));
+		assertEquals(5, legs.size());
+		assertEquals(TransportMode.walk, ((Leg) legs.get(0)).getMode());
+		assertEquals(TransportMode.pt, ((Leg) legs.get(1)).getMode());
+		assertEquals(TransportMode.walk, ((Leg) legs.get(2)).getMode());
+		assertEquals(TransportMode.pt, ((Leg) legs.get(3)).getMode());
+		assertEquals(TransportMode.walk, ((Leg) legs.get(4)).getMode());
+		assertTrue("expected TransitRoute in leg.", ((Leg) legs.get(1)).getRoute() instanceof TransitPassengerRoute);
+		TransitPassengerRoute ptRoute = (TransitPassengerRoute) ((Leg) legs.get(1)).getRoute();
+		assertEquals(Id.create("22", TransitStopFacility.class), ptRoute.getAccessStopId());
+		assertEquals(Id.create("18", TransitStopFacility.class), ptRoute.getEgressStopId());
+		assertEquals(f.greenLine.getId(), ptRoute.getLineId());
+		assertEquals(Id.create("green clockwise", TransitRoute.class), ptRoute.getRouteId());
+		assertTrue("expected TransitRoute in leg.", ((Leg) legs.get(3)).getRoute() instanceof TransitPassengerRoute);
+		ptRoute = (TransitPassengerRoute) ((Leg) legs.get(3)).getRoute();
+		assertEquals(Id.create("4", TransitStopFacility.class), ptRoute.getAccessStopId());
+		assertEquals(Id.create("12", TransitStopFacility.class), ptRoute.getEgressStopId());
+		assertEquals(f.redLine.getId(), ptRoute.getLineId());
+		assertEquals(Id.create("red C > G", TransitRoute.class), ptRoute.getRouteId());
+		double actualTravelTime = 0.0;
+		for (PlanElement leg : legs) {
+			actualTravelTime += ((Leg) leg).getTravelTime().seconds();
+			System.out.println(((Leg) leg).getTravelTime().seconds());
+		}
+		double expectedTravelTime = 3060 + // agent takes the *:06 course, arriving in C at *:18, departing at *:21, arriving in K at*:31
+				CoordUtils.calcEuclideanDistance(f.schedule.getFacilities().get(Id.create("21", TransitStopFacility.class)).getCoord(), toCoord) / raptorParams.getBeelineWalkSpeed();
+		expectedTravelTime = 4310;
+		assertEquals(Math.ceil(expectedTravelTime), actualTravelTime, MatsimTestUtils.EPSILON);
+	}
+
 }
